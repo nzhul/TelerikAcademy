be04d002d294aa08382e3178a04d48924b32f354
nzhul <dobromirivanov1@gmail.com>
2013-05-09 00:13:48 +0300

    operators and expressions exercises

--- CSharpPartOne/3.Operators-Expressions-and-Statements/05-bit3/05-bit3.cs
+++ CSharpPartOne/3.Operators-Expressions-and-Statements/05-bit3/05-bit3.cs
@@ -6,6 +6,27 @@ class bit3
 {
     static void Main()
     {
+        int number = int.Parse(Console.ReadLine());
+        int mask = 8;
+        //mask = mask << 3;
+
+        int addMask = number & mask;
+
+        if (addMask != 0)
+        {
+            Console.WriteLine("The 3th bit is 1");
+        }
+        else {
+            Console.WriteLine("The 3th bit is 0"); // It isn't 1 
+        }
+
+
+        // Those lines of code helps to understand the bitwise operation
+        string numberBinary = Convert.ToString(number, 2);
+        string maskBinary = Convert.ToString(mask, 2);
+        string addMaskBinary = Convert.ToString(addMask, 2);
+
+        Console.WriteLine("{0} : number\n{1} : mask\n{2} : addMask", numberBinary, maskBinary, addMaskBinary);
 
     }
 }
