2e99128874aaf241b9336df9dc39c76c4c8673cf
nzhul <dobromirivanov1@gmail.com>
2013-05-16 00:24:04 +0300

    05 - Conditional Statements 1

---
+++ CSharpPartOne/05.Conditional-Statements/02-ProductSign/02-ProductSign.cs
@@ -0,0 +1,22 @@
+﻿// 02. Write a program that shows the sign (+ or -) of the product of three real numbers without calculating it. Use a sequence of if statements.
+
+using System;
+
+class ProductSign
+{
+    static void Main()
+    {
+        int a = 5;
+        int b = 2;
+        int c = -6;
+
+        if (a < 0 || b < 0 || c < 0)
+        {
+            Console.WriteLine("The sign of the product is NEGATIVE (-), because one or more of the numbers are negative!");
+        }
+        else
+        {
+            Console.WriteLine("The sign of the product is POSITIVE (+), because none of the numbers are negative!");
+        }
+    }
+}
---
+++ CSharpPartOne/05.Conditional-Statements/03-BiggestInt/03-BiggestInt.cs
@@ -0,0 +1,32 @@
+﻿// 03. Write a program that finds the biggest of three integers using nested if statements.
+
+using System;
+using System.Linq;
+
+class ProductSign
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        if ((a >= b && a >= c))
+        {
+            Console.WriteLine("{0} is the biggest integer.", a);
+        }
+        else if (b >= a && b >= c)
+        {
+            Console.WriteLine("{0} is the biggest integer.", b);
+        }
+        else
+        {
+            Console.WriteLine("{0} is the biggest integer.", c);
+        }
+
+        // Secondary solution
+        int[] numbers = new int[] { 1, 3, 2 };
+        int maximumNumber = numbers.Max();
+        Console.WriteLine("the Maximum number is: {0}",maximumNumber);
+    }
+}
