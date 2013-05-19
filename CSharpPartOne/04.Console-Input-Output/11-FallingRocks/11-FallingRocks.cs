9ffdb920b3edf8615ae3d3c0b963408e19185da8
nzhul <dobromirivanov1@gmail.com>
2013-05-19 14:36:12 +0300

    Falling Rocks update

--- CSharpPartOne/04.Console-Input-Output/11-FallingRocks/11-FallingRocks.cs
+++ CSharpPartOne/04.Console-Input-Output/11-FallingRocks/11-FallingRocks.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// * Implement the "Falling Rocks" game in the text console. A small dwarf stays at the bottom of the 
+// screen and can move left and right (by the arrows keys). A number of rocks of different sizes and 
+// forms constantly fall down and you need to avoid a crash.
+// Rocks are the symbols ^, @, *, &, +, %, $, #, !, ., ;, - distributed with appropriate density.
+// The dwarf is (O). Ensure a constant game speed by Thread.Sleep(150).
+// Implement collision detection and scoring system.
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
