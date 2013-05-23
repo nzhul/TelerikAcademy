70576fd19e9e6bcb5aa3fa0007ec2f2f8f7e5836
nzhul <dobromirivanov1@gmail.com>
2013-05-23 23:15:29 +0300

    SubSet

--- CSharpPartOne/05.Conditional-Statements/09-SubSet/09-SubSet.cs
+++ CSharpPartOne/05.Conditional-Statements/09-SubSet/09-SubSet.cs
@@ -9,7 +9,58 @@ class SubSet
 {
     static void Main()
     {
-        // TODO: Пак трябва да се ползват побитови оператори :(
+        Console.Write("Enter the integer count: ");
+        int intCount = int.Parse(Console.ReadLine());
+        int[] intArray = new int[intCount]; 
+
+        for (int i = 0; i < intCount; i++)
+        {
+            intArray[i] = int.Parse(Console.ReadLine());
+        }
+
+        int sum = 0;
+        int subsetCount = 0;
+
+        for (int i = 0; i < intCount; i++)
+        {
+            for (int j = i + 1; j < intCount; j++)
+            {
+                sum = intArray[i] + intArray[j];
+                if (sum == 0)
+                {
+                    subsetCount++;
+                }
+                Console.WriteLine("{0} + {1} = {2}",i ,j , sum);
+                for (int k = j + 1; k < intCount; k++)
+                {
+                    sum = sum + intArray[k];
+                    if (sum == 0)
+                    {
+                        subsetCount++;
+                    }
+                    Console.WriteLine("{0} + {1} + {2} = {3}", i, j, k, sum);
+                    for (int l = k + 1; l < intCount; l++)
+                    {
+                        sum = sum + intArray[l];
+                        if (sum == 0)
+                        {
+                            subsetCount++;
+                        }
+                        Console.WriteLine("{0} + {1} + {2} + {3} = {4}", i, j, k, l, sum);
+                        for (int m = l + 1; m < intCount; m++)
+                        {
+                            sum = sum + intArray[m];
+                            if (sum == 0)
+                            {
+                                subsetCount++;
+                            }
+                            Console.WriteLine("{0} + {1} + {2} + {3} + {4} = {5}", i, j, k, l, m, sum); 
+                        }
+                    }
+                }
+            }
+        }
+        Console.WriteLine("There are {0} sums of subsets that are equal to Zero!",subsetCount);
     }
 }
 
