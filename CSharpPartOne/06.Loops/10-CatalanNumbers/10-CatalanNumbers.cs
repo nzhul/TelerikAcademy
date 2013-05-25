704f0f094d04b6447cf7c9e6eb2d6c9736d1ad50
nzhul <dobromirivanov1@gmail.com>
2013-05-25 22:32:31 +0300

    All Cards From Deck

---
+++ CSharpPartOne/06.Loops/10-CatalanNumbers/10-CatalanNumbers.cs
@@ -0,0 +1,30 @@
+﻿// 10. In the combinatorial mathematics, the Catalan numbers are calculated by the following formula:
+// (2*n)! / (n + 1)! * n!
+// Write a program to calculate the Nth Catalan number by given N.
+
+// NB! Exercise 9 = Exercise 10
+
+
+using System;
+
+class CatalanNumbers
+{
+    static double Factorial(double n)
+    {
+        double nFact = 1;
+        for (double i = 1; i <= n; i++)
+        {
+            nFact = nFact * i;
+        }
+        return nFact;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter N: ");
+        double n = int.Parse(Console.ReadLine());
+        double catalanNumber = Factorial(2 * n) / Factorial(n + 1) * Factorial(n);
+        Console.WriteLine(catalanNumber);
+    }
+}
+
---
+++ CSharpPartOne/06.Loops/11-AllCardsFromDeck/11-AllCardsFromDeck.cs
@@ -0,0 +1,21 @@
+﻿// 10. In the combinatorial mathematics, the Catalan numbers are calculated by the following formula:
+
+
+using System;
+
+class AllCardsFromDeck
+{
+    static void Main()
+    {
+        string[] ranks = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+        string[] colors = {"Clubs", "Diamonds", "Hearts", "Spades" };
+        foreach (var rank in ranks)
+        {
+            foreach (var color in colors)
+            {
+                Console.WriteLine(rank + " of " + color);
+            }
+        }
+    }
+}
+
