346072ae68ef4d65f54dd84ab31a9a29edff8cf7
nzhul <dobromirivanov1@gmail.com>
2013-05-28 21:47:00 +0300

    Spiral Matrix Complete

--- CSharpPartOne/06.Loops/14-SpiralMatrix/14-SpiralMatrix.cs
+++ CSharpPartOne/06.Loops/14-SpiralMatrix/14-SpiralMatrix.cs
@@ -8,30 +8,71 @@ class SpiralMatrix
 {
     static void Main()
     {
-
-        // !!! ЗАДАЧАТА НЕ Е ГОТОВА!
-        //
-        int n = 5;
+        Console.Write("Enter n: ");
+        int n = int.Parse(Console.ReadLine());
         int[,] matrix = new int[n, n];
+        int row = 0;
+        int col = 0;
+        string direction = "right";
+        int maxRotations = n * n;
 
-        for (int r = 0; r < n; r++)
+        for (int i = 1; i <= maxRotations; i++)
         {
-            for (int c = 0; c < n; c++)
+            if (direction == "right" && (col > n - 1 || matrix[row, col] != 0))
             {
-                matrix[r, c] = c + 1;
+                direction = "down";
+                col--;
+                row++;
+            }
+            if (direction == "down" && (row > n - 1 || matrix[row, col] != 0))
+            {
+                direction = "left";
+                row--;
+                col--;
+            }
+            if (direction == "left" && (col < 0 || matrix[row, col] != 0))
+            {
+                direction = "up";
+                col++;
+                row--;
+            }
+
+            if (direction == "up" && row < 0 || matrix[row, col] != 0)
+            {
+                direction = "right";
+                row++;
+                col++;
+            }
+
+            matrix[row, col] = i;
+
+            if (direction == "right")
+            {
+                col++;
+            }
+            if (direction == "down")
+            {
+                row++;
+            }
+            if (direction == "left")
+            {
+                col--;
+            }
+            if (direction == "up")
+            {
+                row--;
             }
         }
 
-        // Display The Matrix
+        // Display Matrix 
+
         for (int r = 0; r < n; r++)
         {
             for (int c = 0; c < n; c++)
             {
-                Console.Write(matrix[r, c]);
+                Console.Write("{0,4}", matrix[r, c]);
             }
             Console.WriteLine();
         }
-
-
     }
 }
