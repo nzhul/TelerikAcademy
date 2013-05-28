c59f91fe2acb1e7420abceb318b081d79883e81b
nzhul <dobromirivanov1@gmail.com>
2013-05-29 00:10:51 +0300

    Miss Cat

---
+++ CSharpPartOne/Exam/01-CartesianCoordinateSystem/01-CartesianCoordinateSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+class CartesianCoordinateSystem
+{
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+
+        int result = 0;
+
+        if (x == 0 && y == 0)
+        {
+            result = 0;
+        }
+
+        else if (x > 0 && y > 0)
+        {
+            result = 1;
+        }
+
+        else if (x < 0 && y > 0)
+        {
+            result = 2;
+        }
+        else if (x < 0 && y < 0)
+        {
+            result = 3;
+        }
+        else if (x > 0 && y < 0)
+        {
+            result = 4;
+        }
+        else if (x == 0)
+        {
+            result = 5;
+        }
+        else if (y == 0)
+        {
+            result = 6;
+        }
+
+        Console.Write(result);
+    }
+}
---
+++ CSharpPartOne/Exam/02-MissCat/02-MissCat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+    class MissCat
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] cats = new int[10];
+            int currentVote;
+
+            for (int i = 0; i < n; i++)
+			{
+                currentVote = int.Parse(Console.ReadLine());
+                cats[currentVote - 1]++;
+			}
+            int maxCat = Array.IndexOf(cats, cats.Max()) + 1;
+
+            Console.WriteLine(maxCat);
+        }
+    }
