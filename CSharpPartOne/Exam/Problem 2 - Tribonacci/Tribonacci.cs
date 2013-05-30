62cb3574d274f96775cc547947eb213df61b3148
nzhul <dobromirivanov1@gmail.com>
2013-05-30 23:55:21 +0300

    Tribonacci

---
+++ CSharpPartOne/Exam/Problem 2 - Tribonacci/Tribonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Numerics;
+
+class Fibunacci
+{
+    static void Main()
+    {
+        BigInteger firstN = BigInteger.Parse(Console.ReadLine()); ;
+        BigInteger secondN = BigInteger.Parse(Console.ReadLine()); ;
+        BigInteger thirtN = BigInteger.Parse(Console.ReadLine()); ;
+        BigInteger fourtN = 0;
+        BigInteger n = BigInteger.Parse(Console.ReadLine()); ;
+
+        for (int i = 3; i < n; i++)
+        {
+            fourtN = firstN + secondN + thirtN;
+            firstN = secondN;
+            secondN = thirtN;
+            thirtN = fourtN;
+        }
+        Console.WriteLine(fourtN);
+    }
+}
