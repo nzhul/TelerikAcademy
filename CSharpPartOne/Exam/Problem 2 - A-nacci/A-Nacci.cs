ed034ca28009b8db990911e605703a9484e22712
nzhul <dobromirivanov1@gmail.com>
2013-06-11 21:11:36 +0300

    hjk

--- CSharpPartOne/Exam/Problem 2 - A-nacci/A-Nacci.cs
+++ CSharpPartOne/Exam/Problem 2 - A-nacci/A-Nacci.cs
@@ -11,11 +11,8 @@
             int l = Int32.Parse(Console.ReadLine());
 
             int currentLetter = firstLetter;
-            for (int i = 0; i < l; i++)
-            {
-                Console.WriteLine(currentLetter);
-                currentLetter = 
-            }
+
+            // TODO ne e reshena zada4ata
 
 
         }
