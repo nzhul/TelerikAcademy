cc04cf6017936c919ab430c07633b0f22ca6abb2
nzhul <nzhul@bitbucket.org>
2013-06-18 17:36:25 +0300

    DancingBits Start

---
+++ CSharpPartOne/Exam/Problem 4 - DancingBits/DancingBits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+class DancingBits
+{
+    static void Main()
+    {
+        int k = Int32.Parse(Console.ReadLine());
+        int n = Int32.Parse(Console.ReadLine());
+
+        StringBuilder finalString = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+        {
+            finalString.Append(Convert.ToString(int.Parse(Console.ReadLine()), 2));
+        }
+
+        for (int i = 0; i < finalString.Length; i++)
+        {
+
+        }
+
+
+    }
+}
