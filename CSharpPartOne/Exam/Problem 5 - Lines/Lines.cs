f856d81853a7c103e2d0b43da37ca976b8b2b8e4
nzhul <nzhul@bitbucket.org>
2013-06-18 15:11:19 +0300

    Lines 100/100

--- CSharpPartOne/Exam/Problem 5 - Lines/Lines.cs
+++ CSharpPartOne/Exam/Problem 5 - Lines/Lines.cs
@@ -23,16 +23,61 @@
             }
             int longestLine = 0;
             int longestLineCount = 0;
+            int currentLine = 0;
             for (int i = 0; i < 8; i++)
             {
+                currentLine = 0;
                 for (int j = 0; j < 8; j++)
                 {
                     if (playField[i, j] == '1')
                     {
-                        longestLine++;
+                        currentLine++;
+                    }
+                    else
+                    {
+                        currentLine = 0;
+                    }
+                    if (currentLine == longestLine)
+                    {
+                        longestLineCount++;
+                    }
+                    if (currentLine > longestLine)
+                    {
+                        longestLine = currentLine;
+                        longestLineCount = 1;
                     }
                 }
             }
 
+            for (int j = 0; j < 8; j++)
+            {
+                currentLine = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (playField[i, j] == '1')
+                    {
+                        currentLine++;
+                    }
+                    else
+                    {
+                        currentLine = 0;
+                    }
+                    if (currentLine == longestLine)
+                    {
+                        longestLineCount++;
+                    }
+                    if (currentLine > longestLine)
+                    {
+                        longestLine = currentLine;
+                        longestLineCount = 1;
+                    }
+                }
+            }
+            if (longestLine == 1)
+            {
+                longestLineCount = longestLineCount / 2;
+            }
+            Console.WriteLine(longestLine);
+            Console.WriteLine(longestLineCount);
         }
     }
