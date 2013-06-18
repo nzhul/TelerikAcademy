96345ab10165c3cc4e3d8f00ccf29059ef4df35d
nzhul <dobromirivanov1@gmail.com>
2013-06-18 21:33:29 +0300

    DancingBits

--- CSharpPartOne/Exam/Problem 4 - DancingBits/DancingBits.cs
+++ CSharpPartOne/Exam/Problem 4 - DancingBits/DancingBits.cs
@@ -14,12 +14,35 @@ class DancingBits
         {
             finalString.Append(Convert.ToString(int.Parse(Console.ReadLine()), 2));
         }
+        finalString.Insert(0, 'R');
+        finalString.Append('R');
 
-        for (int i = 0; i < finalString.Length; i++)
+        //Console.WriteLine(finalString);
+
+        int dancingCount = 0;
+        int equalCounter = 0;
+        for (int i = 1; i < finalString.Length - 1; i++)
         {
+            if (finalString[i] == finalString[i - 1])
+            {
+                equalCounter++;
+            }
+            else
+            {
+                equalCounter = 0;
+            }
 
+            if (equalCounter == k - 1)
+            {
+                if (i < finalString.Length - 1)
+                {
+                    if (finalString[i] != finalString[i + 1])
+                    {
+                        dancingCount++;
+                    }
+                }
+            }
         }
-
-
+        Console.WriteLine(dancingCount);
     }
 }
