354d4c9c6edf6a66a3bb4f2f3bd3944c04f05ce6
nzhul <dobromirivanov1@gmail.com>
2013-06-25 23:26:24 +0300

    Sequences

---
+++ CSharpPartTwo/01.Arrays/04-MaximalSequence/04-MacimalSequence.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+class MaximalSequence
+{
+    static void Main()
+    {
+        int[] numbers = {2, 1, 1, 2, 3, 3, 2, 2, 2, 1 };
+        int maxSequence = 0;
+        int currentSequence = 0;
+        int SequenceNumber = 0;
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            if (numbers[i] == numbers[i + 1])
+            {
+                currentSequence++;
+            }
+            else
+            {
+                if (currentSequence > maxSequence)
+                {
+                    maxSequence = currentSequence;
+                    SequenceNumber = numbers[i];
+                }
+                currentSequence = 1;
+            }
+        }
+
+        // Special case
+        if (currentSequence > maxSequence)
+        {
+            maxSequence = currentSequence;
+            SequenceNumber = numbers[numbers.Length - 1];
+        }
+
+        // Display the maximum sequence
+        Console.WriteLine("The input sequence is:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("{0} ", numbers[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("The maximum sequence is: ");
+        for (int i = 0; i < maxSequence; i++)
+        {
+            Console.Write("{0} ", SequenceNumber);
+        }
+    }
+}
---
+++ CSharpPartTwo/01.Arrays/05-MaxIncreasingSequence/05-MaxIncreasingSequence.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+class MaxIncreasingSequence
+{
+    static void Main()
+    {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 2, 5, 6, 7, 8, 9, 10, 11, 12, 2, 3, 4, 4 };
+        int maxSequence = 0;
+        int currentSequence = 0;
+        int sequenceEnd = 0;
+
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            if (numbers[i] + 1 == numbers[i + 1])
+            {
+                currentSequence++;
+            }
+            else
+            {
+                if (currentSequence > maxSequence)
+                {
+                    sequenceEnd = numbers[i];
+                    maxSequence = currentSequence;
+                }
+                currentSequence = 1;
+            }
+        }
+
+        // Special Case
+        if (currentSequence > maxSequence)
+        {
+            maxSequence = currentSequence;
+            sequenceEnd = numbers[numbers.Length - 1];
+        }
+
+        // Display the maximum sequence
+        Console.WriteLine("The input sequence is:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("{0} ", numbers[i]);
+        }
+        Console.WriteLine();
+        Console.WriteLine("The maximum Increasing sequence is: ");
+        for (int i = maxSequence - 1; i >= 0; i--)
+        {
+            Console.Write("{0} ", sequenceEnd - i);
+        }
+    }
+}
+
+// There is a problem if the sequence start from the 1st element. // TODO
