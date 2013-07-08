ac60586335c0d85f6709bf909a713ea0b7c9c665
nzhul <nzhul@bitbucket.org>
2013-07-08 17:31:15 +0300

    update

---
+++ CSharpPartTwo/01.Arrays/20-AllVariations/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class AllVariations
+{
+    static void Main()
+    {
+        int n = Int32.Parse(Console.ReadLine());
+        int k = Int32.Parse(Console.ReadLine());
+        
+    }
+}
