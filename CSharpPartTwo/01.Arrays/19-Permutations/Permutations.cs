e2969270d93d7a391f74aa25c44d9bd4e91ae589
nzhul <nzhul@bitbucket.org>
2013-07-09 17:29:24 +0300

    Permutations
    The Task is not Complete!

---
+++ CSharpPartTwo/01.Arrays/19-Permutations/Permutations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Permutations
+{
+    static void Main()
+    {
+        char[] elements = { 'a', 'b', 'c' };
+
+        for (int i = 0; i < elements.Length; i++)
+        {
+            for (int j = 0; j < elements.Length; j++)
+            {
+                for (int z = 0; z < elements.Length; z++)
+                {
+                    if (elements[i] != elements[j] && elements[i] != elements[z] && elements[j] != elements[z] )
+                    {
+                        Console.WriteLine("{0}, {1}, {2}", elements[i], elements[j], elements[z]);
+                    }
+                }
+            }
+        }
+    }
+}
