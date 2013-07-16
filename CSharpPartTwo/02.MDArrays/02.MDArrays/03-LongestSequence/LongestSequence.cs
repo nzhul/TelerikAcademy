faef19368b0d71bac534318f0aed1c07c5160e9a
Dobromir Ivanov <dobromirivanov1@gmail.com>
2013-07-16 11:33:39 -0700

    Update LongestSequence.cs

--- CSharpPartTwo/02.MDArrays/02.MDArrays/03-LongestSequence/LongestSequence.cs
+++ CSharpPartTwo/02.MDArrays/02.MDArrays/03-LongestSequence/LongestSequence.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 
 class LongestSequence
@@ -11,10 +11,32 @@ class LongestSequence
             {"fo", "ha", "hi", "xx"},
             {"xxx", "ho", "ha", "xx"}
         };
-        int currentSeq = 0;
-        int bestSeq = 0;
+        int currentSeq = 1;
+        int bestSeq = int.MinValue;
         int bestSeqRow = 0;
         int bestSeqCol = 0;
         int bestSeqDirection = 0;
+
+        for (int i = 0; i < elements.GetLength(0); i++)
+        {
+            for (int j = 0; j < elements.GetLength(1) - 1; j++)
+            {
+                if (elements[i, j] == elements[i, j + 1])
+                {
+                    currentSeq++;
+                }
+                else
+                {
+                    currentSeq = 1;
+                }
+
+                if (bestSeq > currentSeq)
+                {
+                    
+                }
+            }
+        }
+
+
     }
 }
