596d7406f95ffd0e8240583b2c3fa33a078caa60
nzhul <dobromirivanov1@gmail.com>
2013-07-17 23:09:14 +0300

    TFS Folder

---
+++ ConsoleGame/Console Game - Just Jewels/GameFolder/GameFolder/JustJewels-Core.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test");
+            Console.WriteLine("Test2");
+        }
+    }
+}
