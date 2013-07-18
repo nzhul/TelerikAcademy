dcb7add51440dc56351fb705e5521a7a0fa3e5b8
nzhul <nzhul@bitbucket.org>
2013-07-18 21:04:46 +0300

    05 - SortingArray

--- CSharpPartTwo/02.MDArrays/05-SortStringArray/SortStringArray.cs
+++ CSharpPartTwo/02.MDArrays/05-SortStringArray/SortStringArray.cs
@@ -5,9 +5,18 @@ class SortStringArray
     static void Main()
     {
         string[] strings = { "12", "12345", "123456", "1", "1234", "123" };
+        string[] sortedArray = SortingMethod(ref strings);
 
+        foreach (var item in sortedArray)
+        {
+            Console.WriteLine(item);
+        }
+    }
 
-
-
+    static string[] SortingMethod(ref string[] array)
+    {
+        string[] sortedArray = (string[])array.Clone();
+        Array.Sort(sortedArray);
+        return sortedArray;
     }
 }
