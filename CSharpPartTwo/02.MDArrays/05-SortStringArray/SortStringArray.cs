126ea8d977c22f18a6cde0eecea89b9d68408a9c
nzhul <dobromirivanov1@gmail.com>
2013-07-19 00:30:29 +0300

    update

--- CSharpPartTwo/02.MDArrays/05-SortStringArray/SortStringArray.cs
+++ CSharpPartTwo/02.MDArrays/05-SortStringArray/SortStringArray.cs
@@ -5,21 +5,21 @@ class SortStringArray
     {
         //string[] strings = { "12", "12345", "123456", "1", "1234", "123" };
         string[] strings = { "azmodan", "belial", "mephisto", "diablo", "assassin", "monk" };
-        SortingMethod(ref strings);
+        SortingMethod(strings);
         foreach (var item in strings)
         {
             Console.WriteLine(item);
         }
     }
 
-    static void SortingMethod(ref string[] array)
+    static void SortingMethod(string[] array)
     {
         int[] stringsLenghts = new int[array.Length];
         for (int i = 0; i < array.Length; i++)
         {
             stringsLenghts[i] = array[i].Length;
         }
-       Array.Sort(stringsLenghts, array);
+        Array.Sort(stringsLenghts, array);
        return;
     }
 }
