bacdbd64797a2fd8ab1fe8b381ffdaa50ad7cbd5
nzhul <nzhul@bitbucket.org>
2013-07-18 12:53:07 +0300

    Bejeweled Start

--- ConsoleGame/Console Game - Just Jewels/GameFolder/GameFolder/JustJewels-Core.cs
+++ ConsoleGame/Console Game - Just Jewels/GameFolder/GameFolder/JustJewels-Core.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameFolder
+namespace ConsoleApplication1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
-            Console.WriteLine("Test2");
+            Cell asd = new Cell(4, 3, '#', ConsoleColor.Green);
+            asd.DrawCell();
+        }
+    }
+
+    public class Cell
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public char Symbol { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Cell(int width, int height, char symbol, ConsoleColor color)
+        {
+            this.Width = width;
+            this.Height = height;
+            this.Symbol = symbol;
+            this.Color = color;
+        }
+
+        public void DrawCell()
+        {
+            Console.ForegroundColor = Color;
+
+            for (int r = 0; r < Height; r++)
+            {
+                Console.WriteLine(new string(this.Symbol, Width));
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
+
+//int[,][,] matrix = new int[8,8][,];
+//[10:20:25] alexander_tri: tova trqbva da ti e sintaksira
+//[10:20:44] alexander_tri: i posle obhojdash elementite ot matricata s 2 for cikula i za vsqka stoinost si suzdavash vutreshna matrica
+
+//for (int r = 0; r < matrix.GetLength(0); r++)
+//[10:21:43] alexander_tri: for c ----/-- -/--
+//[10:21:57] alexander_tri: matrix[r,c] = new int[3,3];
+//[10:22:06] alexander_tri: ili 4/4.. kakto iskash
+
+//znachi pravish si 2 poleta width i heighth, moje edno za simvol i edno za cvqt.. posle override-vash .ToString() za da znae kak da se risuva i taka
---
+++ ConsoleGame/Console Game - Just Jewels/GameFolder/JewelTests/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrawCell(0, 0, '#', ConsoleColor.Red);
+            DrawCell(5, 0, '@', ConsoleColor.Blue); ;
+        }
+
+        public static void DrawCell(int startingX, int startingY, char symbol, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+
+            for (int r = startingX; r < startingX + 3; r++)
+            {
+                for (int c = startingY; c < startingY + 3; c++)
+                {
+                    Console.SetCursorPosition(r, c);
+                    Console.Write(symbol);
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
