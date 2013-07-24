0d8ef25897f9b9bae6752669bc7f566ab7ce296d
nzhul <dobromirivanov1@gmail.com>
2013-07-25 00:23:03 +0300

    Methods Start

---
+++ CSharpPartTwo/03.Methods/08-BigIntegerMethod/BigInteger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class BigInteger
+{
+    static void Main()
+    {
+        Console.Write("Enter the First Number: ");
+        byte[] numberOne = Console.ReadLine().ToCharArray().Select(c => byte.Parse(c.ToString())).ToArray();
+        Console.Write("Enter the First Number: ");
+        byte[] numberTwo = Console.ReadLine().ToCharArray().Select(c => byte.Parse(c.ToString())).ToArray();
+
+        List<byte> newNumber = new List<byte>();
+
+
+
+
+
+    }
+}
