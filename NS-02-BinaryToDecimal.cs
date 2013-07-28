8f40b2699880378ca7348ae547f62168402ea721
Dobromir Ivanov <dobromirivanov1@gmail.com>
2013-07-28 08:27:23 -0700

    Create NS-02-BinaryToDecimal.cs

---
+++ NS-02-BinaryToDecimal.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+class BinaryToDecimal
+{
+    static void Main()
+    {
+        Console.Write("Enter binary Number: ");
+        byte[] binaryNumber = Console.ReadLine().ToCharArray().Select(c => byte.Parse(c.ToString())).ToArray();
+
+        int decimalNumber = 0;
+        for (int i = 0, j = binaryNumber.Length - 1; i < binaryNumber.Length; i++, j--)
+        {
+            decimalNumber += binaryNumber[i] * (int)Math.Pow(2, j);
+        }
+        Console.WriteLine(decimalNumber);
+    }
+}
