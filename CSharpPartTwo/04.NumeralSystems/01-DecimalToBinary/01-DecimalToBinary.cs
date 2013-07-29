c9b8aef94bad24e7f7cb2d4e8d69493ad7f49fd5
nzhul <nzhul@bitbucket.org>
2013-07-29 15:17:46 +0300

    Numeral Systems Start

---
+++ CSharpPartTwo/04.NumeralSystems/01-DecimalToBinary/01-DecimalToBinary.cs
@@ -0,0 +1,39 @@
+﻿// 01. Write a program to convert decimal numbers to their binary representation.
+
+using System;
+using System.Collections.Generic;
+
+
+class DecimalToBinary
+{
+    static void Main()
+    {
+        Console.Write("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+        List<bool> binaryNumber = new List<bool>();
+
+        while (n != 0)
+        {
+            if (n % 2 == 0)
+            {
+                binaryNumber.Add(false);
+            }
+            else
+            {
+                binaryNumber.Add(true);
+            }
+            n /= 2;
+        }
+        for (int i = binaryNumber.Count - 1; i >= 0; i--)
+        {
+            if (binaryNumber[i])
+            {
+                Console.Write(1);
+            }
+            else
+            {
+                Console.Write(0);
+            }
+        }
+    }
+}
---
+++ CSharpPartTwo/04.NumeralSystems/03-DecimalToHex/03-DecimalToHex.cs
@@ -0,0 +1,51 @@
+﻿// 03. Write a program to convert decimal numbers to their hexadecimal representation.
+
+
+using System;
+using System.Collections.Generic;
+
+
+class DecimalToHex
+{
+    static void Main()
+    {
+        Console.Write("Decimal: ");
+        int n = int.Parse(Console.ReadLine());
+        List<byte> hexNumber = new List<byte>();
+
+        while (n != 0)
+        {
+            hexNumber.Add((byte)(n % 16));
+            n /= 16;
+        }
+        Console.Write("Hexadecimal: ", n);
+        for (int i = hexNumber.Count - 1; i >= 0; i--)
+        {
+            switch (hexNumber[i])
+            {
+                case 10:
+                    Console.Write('A');
+                    break;
+                case 11:
+                    Console.Write('B');
+                    break;
+                case 12:
+                    Console.Write('C');
+                    break;
+                case 13:
+                    Console.Write('D');
+                    break;
+                case 14:
+                    Console.Write('E');
+                    break;
+                case 15:
+                    Console.Write('F');
+                    break;
+                default:
+                    Console.Write(hexNumber[i]);
+                    break;
+            }
+        }
+        Console.WriteLine();
+    }
+}
