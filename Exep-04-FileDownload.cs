d090dbb74c8541d15c9ba05142814a1ae8ffbb0a
Dobromir Ivanov <dobromirivanov1@gmail.com>
2013-08-11 10:11:44 -0700

    Update Exep-04-FileDownload.cs

--- Exep-04-FileDownload.cs
+++ Exep-04-FileDownload.cs
@@ -7,12 +7,12 @@ class FileDownload
     static void Main()
     {
         Console.WriteLine("Enter the URL of the which you want to download:\nExample: \"http://www.d3bg.org/img/upload/tamplier/01.jpg\" ");
-        string URL = Console.ReadLine();
-        string fileName = GetFileName(URL);
+        Uri uri = new Uri(Console.ReadLine());
+        string fileName = System.IO.Path.GetFileName(uri.LocalPath);
         WebClient webClient = new WebClient();
         try
         {
-            webClient.DownloadFile(URL, fileName);
+            webClient.DownloadFile(uri, fileName);
             Console.WriteLine("The file was successfully downloaded!\nPlease check bin/Debug Directory!");
         }
         catch (WebException)
@@ -27,21 +27,5 @@ class FileDownload
         {
             webClient.Dispose();
         }
-    }
-
-    private static string GetFileName(string URL)
-    {
-        string[] tokens = URL.Split('/');
-        string fileName = "";
-
-        if (tokens.Length > 0)
-        {
-            fileName = tokens[tokens.Length - 1];
-        }
-        else
-        {
-            fileName = URL;
-        }
-        return fileName;
     }
 }
