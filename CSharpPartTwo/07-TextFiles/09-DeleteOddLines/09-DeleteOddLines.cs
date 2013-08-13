bf9cd13690fffce6d8a055e898d450701245b1f8
nzhul <nzhul@bitbucket.org>
2013-08-13 17:29:38 +0300

    Text - Files - DeleteOddLines

---
+++ CSharpPartTwo/07-TextFiles/09-DeleteOddLines/09-DeleteOddLines.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class DeleteOddLines
+{
+    static void Main()
+    {
+        string[] lines = File.ReadAllLines("../../file.txt");
+
+        using (StreamWriter writer = new StreamWriter("../../file.txt"))
+        {
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    writer.WriteLine(lines[i]);
+                }
+            }
+            Console.WriteLine("Complete!");
+        }
+    }
+}
