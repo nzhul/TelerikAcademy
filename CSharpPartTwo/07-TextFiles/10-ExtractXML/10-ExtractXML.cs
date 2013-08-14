94b4e07cf99d63db9207692a78341c8839c8e322
nzhul <nzhul@bitbucket.org>
2013-08-14 18:10:39 +0300

    extract xml

---
+++ CSharpPartTwo/07-TextFiles/10-ExtractXML/10-ExtractXML.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class ExtractXML
+{
+    static void Main()
+    {
+        string line = null;
+        List<string> xmlValues = new List<string>();
+        using (StreamReader reader = new StreamReader("../../XMLFile1.xml"))
+        {
+            while ((line = reader.ReadLine()) != null)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (i < line.Length - 1 && line[i] == '>' && line[i + 1] != '<')
+                    {
+                        int startingIndex = i + 1;
+                        int wordLength = 0;
+                        while (line[i] != '<')
+                        {
+                            wordLength++;
+                            i++;
+                        }
+                        xmlValues.Add(line.Substring(startingIndex, wordLength - 1));
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < xmlValues.Count; i++)
+        {
+            Console.WriteLine("{0}: {1}", i+1, xmlValues[i]);
+        }
+    }
+}
