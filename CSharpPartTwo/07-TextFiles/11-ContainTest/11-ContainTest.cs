182261d37ad0473c09da1eb9b0385ac9b24877ee
nzhul <dobromirivanov1@gmail.com>
2013-08-14 21:46:50 +0300

    11 - test prefix

---
+++ CSharpPartTwo/07-TextFiles/11-ContainTest/11-ContainTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+class ContainTest
+{
+    static void Main()
+    {
+        string fileContent = File.ReadAllText("../../test.txt");
+        File.WriteAllText("../../test.txt",Regex.Replace(fileContent, @"\b(test\w*)\b", String.Empty));
+        Console.WriteLine("Replace Complete!");
+    }
+}
