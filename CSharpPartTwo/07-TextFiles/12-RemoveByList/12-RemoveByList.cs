6a22d6d9e50377e3ed1e221db9051f2d59794b78
nzhul <dobromirivanov1@gmail.com>
2013-08-15 00:13:16 +0300

    Text-Files Complete

---
+++ CSharpPartTwo/07-TextFiles/12-RemoveByList/12-RemoveByList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Security;
+
+class RemoveByList
+{
+    static void Main()
+    {
+        try
+        {
+            string regex = @"\b(" + String.Join("|", File.ReadAllLines("../../words.txt")) + @")\b";
+            string fileContent = File.ReadAllText("../../fileForClearing.txt");
+            File.WriteAllText("../../ClearedFile.txt", Regex.Replace(fileContent, regex, String.Empty,RegexOptions.IgnoreCase));
+            Console.WriteLine("Replace Complete!");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File Not Found!");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            Console.WriteLine("Directory Not Found!");
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Input/Output Error!");
+        }
+        catch (SecurityException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.WriteLine("File Access Error!");
+        }
+    }
+}
---
+++ CSharpPartTwo/07-TextFiles/13-WordsCount/13-WordsCount.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+class WordsCount
+{
+    static void Main()
+    {
+        string[] words = File.ReadAllLines("../../words.txt");
+        Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+        using (StreamReader reader = new StreamReader("../../fileForScaning.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                for (int i = 0; i < words.Length; i++)
+                {
+                    string regex = @"\b" + words[i] + @"\b";
+                    MatchCollection matches = Regex.Matches(line, regex,RegexOptions.IgnoreCase);
+                    if (dictionary.ContainsKey(words[i]))
+                    {
+                        dictionary[words[i]] += matches.Count;
+                    }
+                    else
+                    {
+                        dictionary.Add(words[i], matches.Count);
+                    }
+                }
+            }
+        }
+
+        using (StreamWriter writer = new StreamWriter("../../result.txt"))
+        {
+            foreach (var wordCount in dictionary.OrderByDescending(key => key.Value))
+            {
+                writer.WriteLine("{0} - {1}", wordCount.Key, wordCount.Value);
+            }
+            Console.WriteLine("Count Complete - Please Check result.txt file!");
+        }
+    }
+}
