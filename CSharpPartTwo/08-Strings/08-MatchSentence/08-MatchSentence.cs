ca46ad3718608cec8ef73d259e0d8ed6848d027f
nzhul <nzhul@bitbucket.org>
2013-08-16 17:54:52 +0300

    Text Files - 8 and 9 tasks

---
+++ CSharpPartTwo/08-Strings/08-MatchSentence/08-MatchSentence.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+class MatchSentence
+{
+    static void Main()
+    {
+        string text = "We are living in a yellow submarine. We don't have anything else. Inside the submarine is very tight. So we are drinking all the day. We will move out of it in 5 days.";
+        MatchCollection matchList = Regex.Matches(text, @"([^\.]*\bin\b[^\.]*)");
+        foreach (Match sentence in matchList)
+        {
+            Console.WriteLine(sentence);
+        }
+    }
+}
---
+++ CSharpPartTwo/08-Strings/09-CensureBadWords/09-CensureBadWords.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class CensureBadWords
+{
+    static void Main()
+    {
+        string text = "Microsoft announced its next generation PHP compiler today. It is based on .NET Framework 4.0 and is implemented as a dynamic language in CLR.";
+        string words = "PHP, CLR, Microsoft";
+        string[] wordsArray = words.Split(new char[] {',',' '},StringSplitOptions.RemoveEmptyEntries);
+
+        string regex = @"\b(" + String.Join("|", wordsArray) + @")\b";
+        Console.WriteLine(Regex.Replace(text, regex, m => new String('*', m.Length)));
+    }
+}
