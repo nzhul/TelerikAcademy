d8326e3d741209b2fc4609e49f854d12e923cc9d
nzhul <nzhul@bitbucket.org>
2013-08-16 15:53:22 +0300

    Strings - EncodeDecode

--- CSharpPartTwo/08-Strings/06-20MaxCharacters/06-MaxCharacters.cs
+++ CSharpPartTwo/08-Strings/06-20MaxCharacters/06-MaxCharacters.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿// 06. Write a program that reads from the console a string of maximum 20 characters. 
+//     If the length of the string is less than 20, the rest of the characters should be filled 
+//     with '*'. Print the result string into the console.
+
+using System;
+using System.IO;
 
 class MaxCharacters
 {
     static void Main()
     {
-        //TODO:
-        //Write a program that reads from the console a string of maximum 20 characters. 
-        //If the length of the string is less than 20, the rest of the characters should be filled 
-        //with '*'. Print the result string into the console.
+        while (true)
+        {
+            Console.WriteLine("Please Enter string <= 20 characters");
+            string input = Console.ReadLine();
+            if (input.Length > 20)
+            {
+                continue;
+            }
+            else
+            {
+                Console.WriteLine(input.PadRight(20, '*'));
+                break;
+            }
+        }
     }
 }
