85219442f406208dcc3bf0c812ee9869c364a158
nzhul <nzhul@bitbucket.org>
2013-08-21 11:56:54 +0300

    20-Palindromes

---
+++ CSharpPartTwo/08-Strings/20-Palindrome/20-Palindrome.cs
@@ -0,0 +1,43 @@
+﻿// 20. Write a program that extracts from a given text all palindromes, e.g. "ABBA", "lamal", "exe".
+
+using System;
+using System.Text.RegularExpressions;
+
+class Palindrome
+{
+    static void Main()
+    {
+        string text = 
+@"The most familiar palindromes, in English at least, 
+are character-by-character: The written characters read the same 
+backward as forward. Some examples of common palindromic words: civic,
+radar, level, rotor, kayak, reviver, racecar, redder, madam, malayalam, 
+and refer.";
+
+        string regex = @"\b\w+\b";
+
+        MatchCollection words = Regex.Matches(text, regex);
+
+        foreach (Match word in words)
+        {
+            if (isPalindrome(word))
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+    }
+
+    static bool isPalindrome(Match word)
+    {
+        string wordString = word.ToString();
+        for (int i = 0; i < wordString.Length; i++)
+        {
+            if (wordString[i] != wordString[wordString.Length - i - 1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
