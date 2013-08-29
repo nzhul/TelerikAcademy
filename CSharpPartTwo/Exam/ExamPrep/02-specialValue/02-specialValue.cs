4b618118710ebacc7e84bc3c4eef05e7e3bfe811
nzhul <dobromirivanov1@gmail.com>
2013-08-30 00:15:53 +0300

    02-SpecialValueStart

---
+++ CSharpPartTwo/Exam/ExamPrep/02-specialValue/02-specialValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+class SpecialValue
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[][] rows = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            string[] rowTokens = Console.ReadLine().Split(new char[] { ' ', ',' },StringSplitOptions.RemoveEmptyEntries );
+            rows[i] = new int[rowTokens.Length];
+            for (int j = 0; j < rowTokens.Length; j++)
+            {
+                rows[i][j] = int.Parse(rowTokens[j]);
+            }
+        }
+    }
+}
