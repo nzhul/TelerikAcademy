d6a84a4e4537906b8a59a6c361001d367dba4f49
nzhul <nzhul@bitbucket.org>
2013-09-06 20:33:34 +0300

    PHP Variables 100-100

--- CSharpPartTwo/Exam/ExamPrep/03-3DStars/Program.cs
+++ CSharpPartTwo/Exam/ExamPrep/03-3DStars/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Stars
 {
-    public static char[, ,] cube;
+    //public static char[, ,] cube;
     static void Main()
     {
         string[] rawDimentions = Console.ReadLine().Split();
         int width = int.Parse(rawDimentions[0]);
         int height = int.Parse(rawDimentions[1]);
         int depth = int.Parse(rawDimentions[2]);
-        cube = new char[width, height, depth];
+        char[, ,] cube = new char[width, height, depth];
 
         for (int h = 0; h < height; h++)
         {
@@ -23,6 +25,44 @@ class Stars
                 }
             }
         }
-        Console.WriteLine();
+
+        Dictionary<char, int> stars = new Dictionary<char, int>();
+        for (int h = 1; h < height - 1; h++)
+        {
+            for (int d = 1; d < depth - 1; d++)
+            {
+                for (int w = 1; w < width - 1; w++)
+                {
+                    if (cube[w, h, d] == cube[w, h + 1, d]
+                     && cube[w, h, d] == cube[w, h - 1, d]
+                     && cube[w, h, d] == cube[w + 1, h, d]
+                     && cube[w, h, d] == cube[w - 1, h, d]
+                     && cube[w, h, d] == cube[w, h, d + 1]
+                     && cube[w, h, d] == cube[w, h, d - 1])
+                    {
+                        if (stars.ContainsKey(cube[w, h, d]))
+                        {
+                            stars[cube[w, h, d]] += 1;
+                        }
+                        else
+                        {
+                            stars.Add(cube[w, h, d], 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        int starsCount = 0;
+        foreach (var star in stars)
+        {
+            starsCount += star.Value;
+        }
+        Console.WriteLine(starsCount);
+
+        foreach (var star in stars.OrderBy(m => m.Key))
+        {
+            Console.WriteLine("{0} {1}", star.Key, star.Value);
+        }
     }
 }
---
+++ CSharpPartTwo/Exam/ExamPrep/04-PHPVariables-Automate/04-PHPVariables-Automate.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+enum ParserState
+{
+    Normal,
+    SingleQuote,
+    DoubleQuote,
+    MultiLineComment,
+    SingleLineComment
+}
+
+class PHPVariables
+{
+    static List<string> variables = new List<string>();
+    static ParserState state = ParserState.Normal;
+    static bool addChars = false;
+    static StringBuilder varBuilder = new StringBuilder();
+
+    static void Main()
+    {
+        string input = "";
+        while (input != "?>")
+        {
+            input = Console.ReadLine();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '$')
+                {
+                    if (addChars)
+                    {
+                        AddVariable();
+                    }
+                    switch (state)
+                    {
+                        case ParserState.Normal:
+                            addChars = true;
+                            break;
+                        case ParserState.SingleQuote:
+                            if (i > 0 && input[i - 1] != '\\' || 
+                               (i > 1 && input[i - 1] == '\\' && input[i - 2] == '\\'))
+                            {
+                                addChars = true;
+                            }
+                            break;
+                        case ParserState.DoubleQuote:
+                            if (i > 0 && input[i - 1] != '\\' ||
+                               (i > 1 && input[i - 1] == '\\' && input[i - 2] == '\\'))
+                            {
+                                addChars = true;
+                            }
+                            break;
+                        case ParserState.MultiLineComment:
+                            break;
+                        case ParserState.SingleLineComment:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (input[i] == '"' )
+                {
+                    if (addChars)
+                    {
+                        AddVariable();
+                    }
+                    switch (state)
+                    {
+                        case ParserState.Normal:
+                            // If we are in Normal State and we encounter double Quote
+                            // we set the state to DoubleQuote
+                            state = ParserState.DoubleQuote;
+                            break;
+                        case ParserState.SingleQuote:
+                            break;
+                        case ParserState.DoubleQuote:
+                            // if we are allready in doubleQuote State and we encounter another double quote
+                            // that is not escaped - we set the state to normal
+                            if (i > 0 && input[i - 1] != '\\' || 
+                               (i > 1 && input[i - 1] == '\\' && input[i - 2] == '\\'))
+                            {
+                                state = ParserState.Normal;
+                            }
+                            break;
+                        case ParserState.MultiLineComment:
+                            break;
+                        case ParserState.SingleLineComment:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (input[i] == '\'')
+                {
+                    if (addChars)
+                    {
+                        AddVariable();
+                    }
+
+                    switch (state)
+                    {
+                        case ParserState.Normal:
+                            // If we are in Normal State and we encounter single quote
+                            // we set the state to SingleQuote;
+                            state = ParserState.SingleQuote;
+                            break;
+                        case ParserState.SingleQuote:
+                            // if we are allready in singleQuote State and we encounter another single quote
+                            // that is not escaped - we set the state to normal
+                            if (i > 0 && input[i - 1] != '\\' || 
+                               (i > 1 && input[i - 1] == '\\' && input[i - 2] == '\\'))
+                            {
+                                state = ParserState.Normal;
+                            }
+                            break;
+                        case ParserState.DoubleQuote:
+                            break;
+                        case ParserState.MultiLineComment:
+                            break;
+                        case ParserState.SingleLineComment:
+                            break;
+                        default:
+                            break;
+                    }
+                    
+                }
+                else if (input[i] == '/')
+                {
+                    if (addChars)
+                    {
+                        AddVariable();
+                    }
+                    switch (state)
+                    {
+                        case ParserState.Normal:
+                            // Check if this is start for singleLine Comment
+                            if (i < input.Length - 1 && input[i + 1] == '/')
+                            {
+                                state = ParserState.SingleLineComment;
+                            }
+                            // Check if this is start for MultiLine Comment
+                            if (i < input.Length - 1 && input[i + 1] == '*')
+                            {
+                                state = ParserState.MultiLineComment;
+                            }
+                            break;
+                        case ParserState.SingleQuote:
+                            break;
+                        case ParserState.DoubleQuote:
+                            break;
+                        case ParserState.MultiLineComment:
+                            // check if this is closing tag for multilineComment
+                            if (i > 0 && input[i - 1] == '*')
+                            {
+                                state = ParserState.Normal;
+                            }
+                            break;
+                        case ParserState.SingleLineComment:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else if (input[i] == '#')
+                {
+                    if (addChars)
+                    {
+                        AddVariable();
+                    }
+                    switch (state)
+                    {
+                        case ParserState.Normal:
+                            // if we are in normal state and we encounter # sign
+                            // we set the state to SingleLineComment
+                            state = ParserState.SingleLineComment;
+                            break;
+                        case ParserState.SingleQuote:
+                            break;
+                        case ParserState.DoubleQuote:
+                            break;
+                        case ParserState.MultiLineComment:
+                            break;
+                        case ParserState.SingleLineComment:
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    if (addChars)
+                    {
+                        if (char.IsDigit(input[i]) || char.IsLetter(input[i]) || input[i] == '_')
+                        {
+                            varBuilder.Append(input[i]);
+                        }
+                        else
+                        {
+                            AddVariable();
+                        }
+                    }
+                }
+            }
+            if (state == ParserState.SingleLineComment)
+            {
+                state = ParserState.Normal;
+            }
+            if (addChars)
+            {
+                AddVariable();
+            }
+        }
+
+        Console.WriteLine(variables.Count);
+        variables.Sort(StringComparer.Ordinal);
+        foreach (string variable in variables)
+        {
+            Console.WriteLine(variable);
+        }
+    }
+
+    private static void AddVariable()
+    {
+        string currentVar = varBuilder.ToString();
+
+        if (currentVar != "")
+        {
+            if (!variables.Contains(currentVar))
+            {
+                variables.Add(currentVar);
+            }
+        }
+        varBuilder.Clear();
+        addChars = false;
+    }
+}
