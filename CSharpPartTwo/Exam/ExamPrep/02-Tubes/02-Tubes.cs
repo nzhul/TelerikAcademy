6456a0724e46d6bbef92bff59f88205855a37d24
nzhul <nzhul@bitbucket.org>
2013-09-06 21:29:08 +0300

    tubes start

---
+++ CSharpPartTwo/Exam/ExamPrep/02-Tubes/02-Tubes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Tubes
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int[] tubes = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            tubes[i] = int.Parse(Console.ReadLine());
+        }
+
+
+    }
+}
