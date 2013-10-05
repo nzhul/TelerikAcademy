32dada7536fa66bed734ac3abb0bb63f84538162
nzhul <dobromirivanov1@gmail.com>
2013-10-06 02:49:41 +0300

    Generic Lists Start

--- OOP/02-Defining-Classes-Part-II/GenericList/GenericList.cs
+++ OOP/02-Defining-Classes-Part-II/GenericList/GenericList.cs
@@ -2,8 +2,31 @@
 
 namespace GenericList
 {
-    class GenericList
+    public class GenericList<T> where T : IComparable
     {
+        private const int DefaultCapacity = 10;
+        private T[] list;
+        private int position;
 
+        public GenericList()
+            : this(DefaultCapacity)
+        { }
+
+        public GenericList(int capacity)
+        { 
+            this.list = new T[capacity];
+        }
+
+        public T this[int index]
+        {
+            get { return this.list[index]; }
+            set { this.list[index] = value; }
+        }
+
+        // methods
+        public void AddElement(T element)
+        { 
+            
+        }
     }
 }
