562e0a6c06e4fb00862c75ec003731b807ec49d7
nzhul <dobromirivanov1@gmail.com>
2013-10-14 00:19:15 +0300

    up

---
+++ OOP/03-Ext-Methods-Delegates-Lambda-LINQ/03-StudentNames/03-StudentNamesTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Test
+{
+    public static IEnumerable<Student> FindByName(Student[] students)
+    {
+        var result = from student in students
+                     where student.FirstName.CompareTo(student.SecondName) < 0
+                     select student;
+        return result;
+    }
+
+    static void Main()
+    {
+        Student[] students = new Student[5];
+        students[0] = new Student("Dobromir", "Ivanov");
+        students[1] = new Student("Serafim", "Jichkov");
+        students[2] = new Student("Evdoki", "Malinov");
+        students[3] = new Student("Pesho", "Petrov");
+        students[4] = new Student("Gosho", "Georgiev");
+
+        var searchedStudents = FindByName(students);
+        foreach (var student in searchedStudents)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ OOP/03-Ext-Methods-Delegates-Lambda-LINQ/03-StudentNames/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+public class Student
+{
+    private string firstName;
+    private string secondName;
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set { this.firstName = value; }
+    }
+
+    public string SecondName
+    {
+        get { return secondName; }
+        set { secondName = value; }
+    }
+
+    public Student(string firstName, string secondName)
+    {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append(string.Format("{0}-{1}", this.firstName, this.secondName));
+        return builder.ToString();
+    }
+    
+}
