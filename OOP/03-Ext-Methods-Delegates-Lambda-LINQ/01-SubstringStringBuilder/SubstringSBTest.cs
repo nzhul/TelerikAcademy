634162e6297d0a16f1318a95db5584b4435d0432
nzhul <dobromirivanov1@gmail.com>
2013-10-13 23:51:30 +0300

    03 extension methods start

---
+++ OOP/03-Ext-Methods-Delegates-Lambda-LINQ/01-SubstringStringBuilder/SubstringSBTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+class SubstringSBTest
+{
+    static void Main()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append("The special love I have for you! My baby blue");
+        Console.WriteLine(builder.SubString(12, 4));
+        Console.WriteLine(builder.SubString(33));
+    }
+}
---
+++ OOP/03-Ext-Methods-Delegates-Lambda-LINQ/02-IEnumerableExtensions/02-IEnumerableExtensionsTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class IEnumerableExtensionsTests
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Sum: {0}", numbers.Sum());
+        Console.WriteLine("Product: {0}", numbers.Product());
+        Console.WriteLine("Min: {0}", numbers.Min());
+        Console.WriteLine("Max: {0}", numbers.Max());
+        Console.WriteLine("Average: {0}", numbers.Average());
+    }
+}
