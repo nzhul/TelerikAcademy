461bfa7d17da3e09eb41a8c3370bf0e040765930
nzhul <dobromirivanov1@gmail.com>
2013-10-30 00:21:34 +0200

    06-Common-Type-System Start

---
+++ OOP/06-Common-Type-System/01-StudentOverride/Student.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StudentOverride
+{
+    public class Student
+    {
+
+    }
+}
---
+++ OOP/06-Common-Type-System/01-StudentOverride/TEST.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentOverride
+{
+    class TEST
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
