e6b84ee4d92d2e199107c0cef90cf1b294663ea6
Dobromir Ivanov <dobromirivanov1@gmail.com>
2013-11-07 22:09:21 +0200

    update

---
+++ OOP/07-AcademyPopcorn/07-AcademyPopcorn/UnstoppableBall.cs
@@ -0,0 +1,19 @@
+﻿namespace AcademyPopcorn
+{
+    public class UnstoppableBall : Ball
+    {
+        public new const string CollisionGroupString = "unstoppableBall";
+        public UnstoppableBall(MatrixCoords topLeft, MatrixCoords speed)
+            : base(topLeft, speed)
+        { }
+
+        public override string GetCollisionGroupString()
+        {
+            return UnstoppableBall.CollisionGroupString;
+        }
+
+        public override
+
+
+    }
+}
