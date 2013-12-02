4f2aee43a78b8bab388ba255242bfe17014545b6
nzhul <dobromirivanov1@gmail.com>
2013-12-02 23:37:52 +0200

    up

---
+++ OOP/Exam/OOP-Exam-March-2013-Variant-1/AcademyRPG-nzhul/Ninja.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AcademyRPG
+{
+    public class Ninja : Character, IFighter
+    {
+        private int _realAttackPoints;
+        public int AttackPoints
+        {
+            get { return _realAttackPoints; }
+        }
+
+        public int DefensePoints
+        {
+            get { return int.MaxValue; }
+        }
+
+        public int GetTargetIndex(List<WorldObject> availableTargets)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
