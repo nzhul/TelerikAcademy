80d8365d94f9a6a91f080ef2353b61e47e4eea2a
nzhul <dobromirivanov1@gmail.com>
2014-05-18 12:26:23 +0300

    HQCode

---
+++ HQCode/02-NamingIdentifiers/01-PrintBoolean/01-PrintBoolean/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    // const int MaxCount = 6;
+
+    class BooleanPrinter
+    {
+        public void Print(bool boolean)
+        {
+            string booleanString = boolean.ToString();
+
+            Console.WriteLine(booleanString);
+        }
+    }
+
+    static void Main()
+    {
+        BooleanPrinter booleanPrinter = new BooleanPrinter();
+
+        booleanPrinter.Print(true);
+    }
+}
