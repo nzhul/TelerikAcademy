fccd490d5cd5bc085a238fb279ace7c5eb1ae9ec
nzhul <nzhul@bitbucket.org>
2014-05-27 17:13:28 +0300

    Snake Complete

--- Javascript/Snake/Snake/js/snake.js
+++ Javascript/Snake/Snake/js/snake.js
@@ -8,17 +8,14 @@
     var cellSpacing = 11;
     canvas.style.width = w +'px';
     canvas.style.height = h + 'px';
-    var direction = 'right';
+    var direction;
     var isDead = false;
-
-    // Paint the canvas
-    ctx.strokeStyle = 'white';
-    ctx.strokeRect(0, 0, w, h);
-
-    // Create Snake
     var snakeArr = [];
+    var mainColor = 'white';
+    var foodElement = {};
+    var scoreHolder = document.getElementById('score');
+    var score = scoreHolder.innerHTML | 0;
 
-    
     function createSnake() {
         var length = 5; // Length of the snake
         snakeArr = [];
@@ -29,7 +26,14 @@
             });
         }
     }
-    createSnake();
+
+    function createFood() {
+        foodElement = {
+            x: Math.floor(Math.random() * (w - w / cellWidth) / cellWidth),
+            y: Math.floor(Math.random() * (h - w / cellWidth) / cellWidth),
+            type: 'regular'
+        }
+    }
 
     // Draw the snake
     function Draw() {
@@ -37,16 +41,18 @@
         //Pain the Whole CANVAS to Clear everything
         ctx.fillStyle = 'black';
         ctx.fillRect(0, 0, w, h);
-        ctx.strokeStyle = 'white';
+        ctx.strokeStyle = mainColor;
         ctx.strokeRect(0, 0, w, h);
 
         // Change Direction
+        // BUG: When you press quickly rightArrow + UpArrow while in DOWN direction - the game restarts.
+        // The same happens when direction = 'UP' and we press quickly rightArrow + downArrow.
         document.onkeydown = function () {
             switch (window.event.keyCode) {
                 case 37: // left
                     if (direction != 'right') direction = 'left';
                     break;
-                case 38: // left
+                case 38: // up
                     if (direction != 'down') direction = 'up';
                     break;
                 case 39: // right
@@ -78,29 +84,70 @@
         }
 
         // Check if out of border
-        if (newHeadX > w / cellSpacing - 1
+        if (newHeadX > w / cellSpacing
             || newHeadX == -1
             || newHeadY == -1
-            || newHeadY > h / cellSpacing - 1) {
+            || newHeadY > h / cellSpacing) {
+            init();
             return;
+        }
+
+        // Tail Self Collision Detect
+        for (var i = 1; i < snakeArr.length; i++) {
+            if (snakeArr[i].x == newHeadX && snakeArr[i].y == newHeadY) {
+                init();
+                return;
+            }
+        }
+
+        // Collision detect
+        if (newHeadX == foodElement.x && newHeadY == foodElement.y) {
+            score++;
+            scoreHolder.innerHTML = score;
+            createFood();
+            drawCell(foodElement.x, foodElement.y);
+
+            // Extend the snake tail
+            snakeArr.push({
+                x: foodElement.x,
+                y: foodElement.y
+            });
         }
 
         var tail = snakeArr.pop();
         tail.x = newHeadX;
-        tail.y = newHeadY
+        tail.y = newHeadY;
         snakeArr.unshift(tail);
 
         // Draw the snake
         for (var i = 0; i < snakeArr.length; i++) {
             var bodyElement = snakeArr[i];
-            ctx.fillStyle = 'white';
-            ctx.fillRect(bodyElement.x * cellSpacing, bodyElement.y * cellSpacing, cellWidth, cellWidth);
+            drawCell(bodyElement.x, bodyElement.y);
         }
+
+        drawCell(foodElement.x, foodElement.y);
+
     }
-    Draw();
 
-    gameLoop = setInterval(Draw, 80);
+    function drawCell(x, y) {
+        ctx.fillStyle = mainColor;
+        ctx.fillRect(x * cellSpacing, y * cellSpacing, cellWidth, cellWidth);
+    }
 
 
-
+    function init() {
+        // Paint the canvas initialy
+        ctx.strokeStyle = 'white';
+        ctx.strokeRect(0, 0, w, h);
+        score = 0;
+        scoreHolder.innerHTML = score;
+        direction = 'right';
+        createFood();
+        createSnake();
+        if (typeof gameLoop != 'undefined') {
+            clearInterval(gameLoop);
+        }
+        gameLoop = setInterval(Draw, 80);
+    }
+    init();
 }())
