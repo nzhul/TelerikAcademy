b98d5fa03e9fa31e9393f7bab2285a09aad81c30
nzhul <nzhul@bitbucket.org>
2014-05-27 17:54:12 +0300

    popcorn start

---
+++ Javascript/Popcorn/Popcorn/js/popcorn.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    //Init canvas stuff
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    var w = 450;
+    var h = 450;
+
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, w, h);
+
+    var Ball = {
+        x: 0,
+        y: 0,
+        type: 'regular'
+    }
+
+    var Brick = {
+        x: 0,
+        y: 0,
+        type: 'regular'
+    }
+}())
