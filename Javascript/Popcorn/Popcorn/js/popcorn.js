995b9b17369694edec44d18de56bbdf7ca9041d1
nzhul <nzhul@bitbucket.org>
2014-06-02 17:29:22 +0300

    popcorn progress

--- Javascript/Popcorn/Popcorn/js/popcorn.js
+++ Javascript/Popcorn/Popcorn/js/popcorn.js
@@ -1,9 +1,16 @@
 ﻿(function () {
     //Init canvas stuff
-    var canvas = document.getElementById('canvas');
-    var cx = canvas.getContext('2d');
-    var canvasW = 450;
-    var canvasH = 450;
+    var canvas = document.getElementById('canvas'),
+        cx = canvas.getContext('2d'),
+        canvasW = 450,
+        canvasH = 450,
+        BALL_START_X = 50,
+        BALL_START_Y = 50,
+        BALL_RADIUS = 10,
+        BALL_COLOR = 'red',
+        PADDLE_WIDTH = 100,
+        PADDLE_HEIGHT = 10,
+        PADDLE_COLOR = 'black';
 
     cx.fillStyle = 'white';
     cx.fillRect(0, 0, canvasW, canvasH);
@@ -35,21 +42,25 @@
         this.directionX = directionX,
         this.directionY = directionY,
         this.type = type,
-        this.animateBall = function () {
-            clear();
+        this.drawBall = function () {
             circle(self.x, self.y, radius, color);
-            self.x += self.directionX;
-            self.y += self.directionY;
-            if (self.x + self.radius + self.directionX > canvasW || self.x + self.directionX + self.radius < 0) {
-                self.directionX = -self.directionX;
-            }
-            if (self.y + self.radius + self.directionY > canvasH || self.y + self.directionY + self.radius < 0) {
-                self.directionY = -self.directionY;
-            }
-            self.x += self.directionX;
-            self.y += self.directionY;
-            window.requestAnimationFrame(self.animateBall);
         };
+        this.changeDirection = function () {
+            if (self.x + self.radius >= canvasW) {
+                self.directionX = -directionX;
+            }
+            if (self.x <= self.radius) {
+                self.directionX = directionX;
+            }
+            if (self.y + self.radius > canvasH) {
+                self.directionY = -directionY;
+            }
+            if (self.y <= self.radius) {
+                self.directionY = directionY;
+            }
+            self.x += self.directionX;
+            self.y += self.directionY;
+        }
         return self;
     }
 
@@ -59,8 +70,44 @@
         this.type = type
     }
 
-    var activeBall = new Ball(50, 50, 2, 4, 25, 'red', 'regular');
-    activeBall.animateBall();
+    function Paddle(x, y, width, height, type, color) {
+        var self = this;
+        this.x = x,
+        this.y = y,
+        this.width = width,
+        this.height = height,
+        this.type = type,
+        this.color = color,
+        this.drawPaddle = function () {
+            rect(self.x, self.y, self.width, self.height, self.color);
+        }
+    }
+
+    function engine() {
+        clear();
+        aBall.drawBall();
+        aPaddle.drawPaddle();
+        aBall.changeDirection();
+        paddleCollision(aBall, aPaddle);
+        window.requestAnimationFrame(engine);
+    }
+
+    // TODO FIX ME
+    function paddleCollision(ball, paddle) {
+        if (ball.y + ball.radius >= paddle.y - PADDLE_HEIGHT) {
+            if (ball.x >= paddle.x && ball.x <= paddle.x + PADDLE_WIDTH) {
+                console.log('paddle collision');
+            } else {
+                console.log('dead');
+            }
+        }
+    }
+
+
+    
+    var aBall = new Ball(BALL_START_X, BALL_START_Y, 2, 6, BALL_RADIUS, BALL_COLOR, 'regular');
+    var aPaddle = new Paddle(canvasW / 2 - PADDLE_WIDTH / 2, canvasH - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT, 'regular', PADDLE_COLOR);
+    engine();
 
 
 
