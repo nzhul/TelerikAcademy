7d6e276f430c6f3774834d00acd803aaf2e6a805
Slavi <slavicvetanov@gmail.com>
2014-06-08 16:32:02 +0300

    Add animation loops and move, direction to Circle
    Add method for detecting when hitting a wall or the paddle. It is just  for test should be refactored later.
    Add move, directionX and Y to circle prototype. On the last rows added game loop with timeout and anim with KineticJS. To test and choose one of this tree.

--- Popcorn-Kinetic/js/popcorn-kinetic.js
+++ Popcorn-Kinetic/js/popcorn-kinetic.js
@@ -27,32 +27,41 @@ var layer = new Kinetic.Layer(),
 
 //containerRect gets the location of the gamefield in the window
 var container = document.getElementById('container');
-var containerRect = container.getBoundingClientRect();
+var containerX = Math.round(container.getBoundingClientRect().left);
+
+Kinetic.Circle.prototype.directionX = null;
+Kinetic.Circle.prototype.directionY = null;
+Kinetic.Circle.prototype.move = function () {
+    this.setY(this.attrs.y - this.attrs.directionY);
+    this.setX(this.attrs.x - this.attrs.directionX);
+    
+    return this;
+}
 
 var aBall = new Kinetic.Circle({
-    x: BALL_START_X,
-    y: BALL_START_Y,
+    x: PADDLE_START_X + PADDLE_WIDTH / 2,
+    y: PADDLE_START_Y - PADDLE_HEIGHT - BALL_RADIUS,
     radius: BALL_RADIUS,
-    fill: BALL_COLOR
+    fill: BALL_COLOR,
+    directionX: DIRECTION_X,
+    directionY: DIRECTION_Y
 }).setAttrs({
-        directionX: DIRECTION_X,
-        directionY: DIRECTION_Y,
-        type: 'regular',
-        changeDirection: function(){
-            if (this.getAttr('x') + this.getAttr('radius') >= stage.getWidth()) {
-                this.setAttr('directionX', this.getAttr('directionX')  -DIRECTION_X )
-            }
-            if (this.getAttr('x') <= this.getAttr('radius')) {
-                this.setAttr('directionX', this.getAttr('directionX') + DIRECTION_X )
-            }
-            // no reflection from down
-            if (this.getAttr('y') <= this.getAttr('radius')) {
-                this.setAttr('directionY', this.getAttr('directionY') + DIRECTION_Y )
-            }
-            this.setAttr('x',this.getAttr('x') + this.getAttr('directionX') );
-            this.setAttr('y',this.getAttr('y') + this.getAttr('directionY') );
-        }
-    });
+    type: 'regular',
+    //changeDirection: function () {
+    //    if (this.getAttr('x') + this.getAttr('radius') >= stage.getWidth()) {
+    //        this.setAttr('directionX', this.getAttr('directionX') - DIRECTION_X)
+    //    }
+    //    if (this.getAttr('x') <= this.getAttr('radius')) {
+    //        this.setAttr('directionX', this.getAttr('directionX') + DIRECTION_X)
+    //    }
+    //    // no reflection from down
+    //    if (this.getAttr('y') <= this.getAttr('radius')) {
+    //        this.setAttr('directionY', this.getAttr('directionY') + DIRECTION_Y)
+    //    }
+    //    this.setAttr('x', this.getAttr('x') + this.getAttr('directionX'));
+    //    this.setAttr('y', this.getAttr('y') + this.getAttr('directionY'));
+    //}
+});
 
 var aPaddle = new Kinetic.Rect({
     x: PADDLE_START_X,
@@ -63,27 +72,61 @@ var aPaddle = new Kinetic.Rect({
     listening: true
 });
 
+//Can add this method to ball or circle object. Now it is just for testing.
+function ballHitWallDetection(ball) {
+    var ballX = ball.attrs.x,
+        ballY = ball.attrs.y,
+        ballRadius = ball.attrs.radius;
 
+    if (ballX + ballRadius >= stage.attrs.width) {
+        ball.attrs.directionX *= -1;
+    }
+    else if (ballX <= ballRadius) {
+        ball.attrs.directionX *= -1;
+    }
+    //if (self.y + self.radius > canvasH) {
+    //    self.directionY = -directionY;
+    //}
+    if (ballY <= ballRadius) {
+        ball.attrs.directionY *= -1;
+    }
+    else if (ballY + ballRadius >= PADDLE_START_Y) {
+        if (ballX >= aPaddle.attrs.x && ballX <= aPaddle.attrs.x + PADDLE_WIDTH ) {
+            ball.attrs.directionY *= -1;
+        }
+        //else {
+        //    GAME_OVER = true;
+        //}
+    }
+}
+
+var prevDate = new Date().getTime();
 
 window.addEventListener('mousemove', function (ev) {
-    var mouseX = ev.clientX;
-    if (mouseX < containerRect.left) {
-        aPaddle.setPosition({
-            x: 0,
-            y: aPaddle.getY()
-        });
-    } 
-    else if (mouseX + PADDLE_WIDTH < stage.getWidth() + containerRect.left) {
-        aPaddle.setPosition({
-            x: ev.clientX - containerRect.left,
-            y: aPaddle.getY()
-        });
-    } 
-    else {
-        aPaddle.setPosition({
-            x: stage.getWidth() - PADDLE_WIDTH,
-            y: aPaddle.getY()
-        });
+    //Try to reduce mousecalls to fix animation lag on move
+    var date = new Date().getTime();
+    if (date - prevDate > 10) {
+        
+        var mouseX = ev.clientX;
+        if (mouseX < containerX) {
+            aPaddle.setPosition({
+                x: 0,
+                y: aPaddle.getY()
+            });
+        } 
+        else if (mouseX + PADDLE_WIDTH < stage.getWidth() + containerX) {
+            aPaddle.setPosition({
+                x: ev.clientX - containerX,
+                y: aPaddle.getY()
+            });
+        } 
+        else {
+            aPaddle.setPosition({
+                x: stage.getWidth() - PADDLE_WIDTH,
+                y: aPaddle.getY()
+            });
+        }
+        prevDate = date;
     }
 });
 
@@ -91,11 +134,31 @@ layer.add(aBall);
 layer.add(aPaddle);
 stage.add(layer);
 
+var anim = new Kinetic.Animation(function (frame) {
+   //For performance test
+   //console.log(frame.frameRate); 
+   
+   aBall.move();
+   ballHitWallDetection(aBall);
+}, layer);
+//
+//function gameLoop() {
+//    setTimeout(gameLoop, 1000 / 60);
+//    //Process movement, AI, game logic
+//    aBall.move();
+//    ballHitWallDetection(aBall);
+//    layer.draw();
+//}
 
-function engine() {
-    aBall.changeDirection;
-    layer.draw();
-    window.requestAnimationFrame(engine);
-}
 
-engine();
+//function engine() {
+//    aBall.move();
+//    ballHitWallDetection(aBall);
+//    layer.draw();
+//    window.requestAnimationFrame(engine);
+//}
+//
+
+anim.start();
+//gameLoop();
+//engine();
