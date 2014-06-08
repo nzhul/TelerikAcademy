0aaff30c8619bd4ac17225ef6bdd84289b068947
nzhul <dobromirivanov1@gmail.com>
2014-06-08 22:06:57 +0300

    super mario start

---
+++ Javascript/14-Animations-1/js/mario.js
@@ -0,0 +1,8 @@
+/**
+ * Created by nzhul on 6/8/2014.
+ */
+var canvas = document.getElementById('canvas');
+var cx = canvas.getContext('2d');
+cx.rect(0,0,50,50);
+cx.fillStyle = 'black';
+cx.fill();
