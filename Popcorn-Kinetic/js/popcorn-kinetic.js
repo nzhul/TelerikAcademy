b616b47a6d643c8e582dce4788be666b2136e6e4
jasonbashov <jbashov@gmail.com>
2014-06-10 00:01:05 +0300

    Explosion Effects
    Added explosion effects when we clear a brick and a sound effect of the
    explosion. Accidently "refreshed" the kinetic library

--- Popcorn-Kinetic/js/popcorn-kinetic.js
+++ Popcorn-Kinetic/js/popcorn-kinetic.js
@@ -29,6 +29,126 @@ stage = new Kinetic.Stage({
 });
 layer = new Kinetic.Layer();
 
+
+//we create new layer for the explosions animation
+var explosionLayer = new Kinetic.Layer();
+//get the underlaying context from the layer:
+var ctx = explosionLayer.canvas.context._context;
+
+//the collection of the explosion particles
+var particles = [];
+
+//we need this for the explosion animations
+var frameRate = 50.0;
+var frameDelay = 1000.0 / frameRate;
+
+
+
+function update(frameDelay) {
+    // draw a white background to clear canvas
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // update and draw particles
+    for (var i = 0; i < particles.length; i++) {
+        var particle = particles[i];
+
+        particle.update(frameDelay);
+        particle.draw(ctx);
+    }
+}
+
+function explode() {
+    var x = aBall.attrs.x;
+    var y = aBall.attrs.y;
+
+    createExplosion(x, y, "#525252");
+
+    createExplosion(x, y, "#FFA318");
+
+    function randomFloat(min, max) {
+        return min + Math.random() * (max - min);
+    }
+
+    function Particle() {
+        this.scale = 1.0;
+        this.x = 0;
+        this.y = 0;
+        this.radius = 20;
+        this.color = "#000";
+        this.velocityX = 0;
+        this.velocityY = 0;
+        this.scaleSpeed = 0.5;
+
+        this.update = function (ms) {
+            // shrinking
+            this.scale -= this.scaleSpeed * ms / 1000.0;
+            if (this.scale <= 0) {
+                this.scale = 0;
+            }
+
+            // moving away from explosion center
+            this.x += this.velocityX * ms / 1000.0;
+            this.y += this.velocityY * ms / 1000.0;
+        };
+
+        this.draw = function (ctx) {
+            // translating the 2D context to the particle coordinates
+            ctx.save();
+            ctx.translate(this.x, this.y);
+            ctx.scale(this.scale, this.scale);
+
+            // drawing a filled circle in the particle's local space
+            ctx.beginPath();
+            ctx.arc(0, 0, this.radius, 0, Math.PI * 2, true);
+            ctx.closePath();
+
+            ctx.fillStyle = this.color;
+            ctx.fill();
+
+            ctx.restore();
+        };
+    }
+    function createExplosion(x, y, color) {
+        var minSize = 10;
+        var maxSize = 30;
+        var count = 10;
+        var minSpeed = 60.0;
+        var maxSpeed = 200.0;
+        var minScaleSpeed = 1.0;
+        var maxScaleSpeed = 4.0;
+
+
+        for (var angle = 0; angle < 360; angle += Math.round(360 / count)) {
+            var particle = new Particle();
+
+            particle.x = x;
+            particle.y = y;
+
+            particle.radius = randomFloat(minSize, maxSize);
+
+            particle.color = color;
+
+            particle.scaleSpeed = randomFloat(minScaleSpeed, maxScaleSpeed);
+
+            var speed = randomFloat(minSpeed, maxSpeed);
+
+            particle.velocityX = speed * Math.cos(angle * Math.PI / 180.0);
+            particle.velocityY = speed * Math.sin(angle * Math.PI / 180.0);
+
+            particles.push(particle);
+        }
+    }
+    //createBasicExplosion(x, y);
+    //requestAnimationFrame(up(frameDelay));
+    //setInterval(function () {
+    //   update(frameDelay);
+    //   console.log(frameDelay);
+    //}, frameDelay);
+    //setTimeout(up(frameDelay), 1000)
+}
+
+
 //containerRect gets the location of the gamefield in the window
 var container = document.getElementById('container');
 var containerX = Math.round(container.getBoundingClientRect().left) + PADDLE_WIDTH / 2; // !
@@ -61,6 +181,10 @@ var aPaddle = new Kinetic.Rect({
     fill: PADDLE_COLOR,
     listening: true
 });
+
+function playSingleSound() {
+    document.getElementById('audiotag1').play();
+}
 
 function initBricks() {
     var brickMatrix = [],
@@ -101,7 +225,10 @@ function ballHitBrickDetection(ball) {
     
     if (colisionObject) {
         console.log(colisionObject);
+        playSingleSound();
         colisionObject.remove();
+        explode();
+        ctx.fillRect(ball.getX(), ball.getY, 50, 50);
         ball.attrs.directionY *= -1;
     }
 }
@@ -166,16 +293,17 @@ window.addEventListener('mousemove', function (ev) {
 
 layer.add(aBall);
 layer.add(aPaddle);
-stage.add(layer);
+stage.add(explosionLayer).add(layer);
 
 var anim = new Kinetic.Animation(function (frame) {
     //For performance test
     //console.log(frame.frameRate);
-
+    //ctx.fillRect(10, 10, 100, 100);
     aBall.move();
     ballHitWallDetection(aBall);
     ballHitBrickDetection(aBall);
-}, layer);
+    update(frameDelay);
+}, layer , explosionLayer);
 //
 //function gameLoop() {
 //    setTimeout(gameLoop, 1000 / 60);
