8f53857e52c7a0cdc52e18edfd529ca7de577dab
nzhul <dobromirivanov1@gmail.com>
2014-06-11 00:47:57 +0300

    JS Events - Todo App

--- Javascript/14-Animations-1/js/mario.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nzhul on 6/8/2014.
- */
-var canvas = document.getElementById('canvas');
-var cx = canvas.getContext('2d');
-cx.rect(0,0,50,50);
-cx.fillStyle = 'black';
-cx.fill();
