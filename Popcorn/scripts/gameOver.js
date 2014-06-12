18759b246abaae26143210e63fb6ac227f36cf2b
georgipyaramov <georgipyaramov@gmail.com>
2014-06-13 01:26:17 +0300

    Click to start and stop the animation.

--- Popcorn/scripts/gameOver.js
+++ Popcorn/scripts/gameOver.js
@@ -1,11 +1,13 @@
 var paper = Raphael(document.getElementById('svg-container'), STAGE_WIDTH, STAGE_HEIGHT + 4),
-    gameOverText = paper.text(250, 160, 'Game Over');
+    gameOverText = paper.text(250, 160, 'Game Over'),
+    textAnimBigger,
+    isAnimationRunning = true;
 
 //    var canvasEl = document.getElementsByClassName('kineticjs-content');
 //
 //    canvasEl.style.display = 'none !important';
 
-    var textAnimBigger = Raphael.animation({
+    textAnimBigger = Raphael.animation({
         '40%': {'font-size': 95, fill: 'darkred', easing: 'bounce'},
         '100%': {'font-size': 10, easing: 'bounce'}
     }, 1000).repeat(Infinity);
@@ -26,10 +28,18 @@ var paper = Raphael(document.getElementById('svg-container'), STAGE_WIDTH, STAGE
 //    });
 
     gameOverText.click(function(){
-        gameOverText.stop();
-        gameOverText.attr({
-            'font-size': 95,
-            fill: 'darkred'
-        })
+        if (isAnimationRunning) {
+            gameOverText.stop();
+            gameOverText.attr({
+                'font-size': 95,
+                fill: 'darkred'
+            });
+            isAnimationRunning = false;
+        }
+        else {
+            gameOverText.animate(textAnimBigger);
+            isAnimationRunning = true;
+        }
+
     });
 
