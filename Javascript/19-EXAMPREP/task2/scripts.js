0c8aa23365f26f043bf683dc700ce816c4b6ac29
nzhul <nzhul@bitbucket.org>
2014-06-16 17:16:33 +0300

    up

--- Javascript/19-EXAMPREP/task2/scripts.js
+++ Javascript/19-EXAMPREP/task2/scripts.js
@@ -1,3 +1,19 @@
 $.fn.tabs = function () {
+    var $container = this;
+    $container.addClass('tabs-container');
+
+    var $allBtns = $('.tab-item-title');
+    $allBtns.on('click', function () {
+       var $currentBtn = $(this);
+        $currentBtn.parent().addClass('current');
+    });
+
+
+
+//
+//
+//    var allTabs = $container.find('.tab-item-content');
+//    allTabs.hide();
+//    $(allTabs[0]).show();
 
 };
