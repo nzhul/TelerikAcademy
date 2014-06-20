508fea9d724b835d5c640e0858418decfd8ecb42
nzhul <nzhul@bitbucket.org>
2014-06-20 16:59:28 +0300

    up heroes

--- Javascript/GAMES/Heroes/js/CONSTANTS.js
+++ Javascript/GAMES/Heroes/js/CONSTANTS.js
@@ -1,5 +1,13 @@
 /**
  * Created by kakadu5 on 20.6.2014 г..
  */
-var mapLayer_WIDTH = 500,
-    mapLayer_HEIGHT = 500;
+
+    // Map
+var mapLayer_WIDTH = 800,
+    mapLayer_HEIGHT = 500,
+    /* Player */
+    player_SIZE = {
+        w: 20,
+        h: 20
+    },
+    player_COLOR = 'yellowgreen';
