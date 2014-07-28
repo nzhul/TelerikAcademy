39c47bbf8cefdec362418ef682b3127a1e9393e0
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-07-28 18:19:23 +0300

    up Chat

---
+++ Javascript/32-JS-APPS-SPA-Apps-Chat/scripts/libs/requestModule.js
@@ -0,0 +1,33 @@
+define(['jquery'], function () {
+    var requestModule = (function () {
+
+        function getJSON(url, success, error) {
+            $.ajax({
+                url: url,
+                type: 'GET',
+                contentType: 'application/json',
+                timeout: 5000,
+                success: success,
+                error: error
+            })
+        }
+
+        function postJSON(url, data, success, error) {
+            $.ajax({
+                url: url,
+                type: 'POST',
+                contentType: 'application/json',
+                timeout: 5000,
+                data: JSON.stringify(data),
+                success: success,
+                error: error
+            })
+        }
+
+        return {
+            getJSON: getJSON,
+            postJSON: postJSON
+        }
+    }());
+    return requestModule;
+});
---
+++ Javascript/32-JS-APPS-SPA-Apps-Chat/scripts/main.js
@@ -0,0 +1,13 @@
+(function () {
+    require.config({
+        paths: {
+            'jquery' : 'libs/jquery-2.1.1.min',
+            'sammy' : 'libs/sammy',
+            'requestModule' : 'libs/requestModule'
+        }
+    });
+
+    require([], function () {
+
+    })
+}());
