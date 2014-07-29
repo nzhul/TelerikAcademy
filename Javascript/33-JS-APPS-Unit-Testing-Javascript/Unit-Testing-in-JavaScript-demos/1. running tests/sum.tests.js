7a0be69cbd061c7c61ba1fbc7d25e99149533a9c
nzhul <nzhul@bitbucket.org>
2014-07-29 14:14:38 +0300

    up

---
+++ Javascript/33-JS-APPS-Unit-Testing-Javascript/Unit-Testing-in-JavaScript-demos/1. running tests/sum.tests.js
@@ -0,0 +1,22 @@
+(function () {
+  describe('#sum', function () {
+    it('when empty array, expect to return 0', function () {
+      var actual = sum([]);
+      var expected = 0;
+      expect(actual).to.equal(expected);
+    });
+    it('when with single number, expect the number', function () {
+      var number = 6;
+      var actual = sum([number]);
+      var expected = number;
+      expect(actual).to.equal(expected);
+    });
+    it('when normal array returns the sum of all elements', function () {
+       var array = [1,2,3,4,5];
+       var expectedResult = 15;
+        var actual = sum(array);
+        expect(actual).to.equal(expectedResult);
+    });
+  });
+
+}());
