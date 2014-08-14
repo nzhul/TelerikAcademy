2bf19563566515c9a34e8eb1d88c32e43e21e5a2
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-14 22:06:22 +0300

    Tree Traversal Homework

---
+++ DataStructures&Algorithms/02-TreesAndTraversals/01-TreeTraversal/01-TreeTraversal.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTraversal
+{
+    class Program
+    {
+        static Node<int> FindRoot(Node<int>[] nodes)
+        {
+            var hasParent = new bool[nodes.Length];
+
+            foreach (var node in nodes)
+            {
+                foreach (var child in node.Children)
+                {
+                    hasParent[child.Value] = true;
+                }
+            }
+
+            for (int i = 0; i < hasParent.Length; i++)
+            {
+                if (!hasParent[i])
+                {
+                    return nodes[i];
+                }
+            }
+
+            throw new ArgumentException("nodes", "The tree has no root.");
+        }
+
+        static void Main(string[] args)
+        {
+            var N = int.Parse(Console.ReadLine());
+            var nodes = new Node<int>[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                nodes[i] = new Node<int>(i);
+            }
+
+            for (int i = 1; i <= N - 1; i++)
+            {
+                string edgeAsString = Console.ReadLine();
+                var edgeParts = edgeAsString.Split(' ');
+
+                int parentId = int.Parse(edgeParts[0]);
+                int childId = int.Parse(edgeParts[1]);
+
+                nodes[parentId].Children.Add(nodes[childId]);
+                nodes[childId].HasParent = true;
+            }
+
+            // 1. Find the root
+            var root = FindRoot(nodes);
+            Console.WriteLine("The root of the tree is: {0}", root.Value);
+
+            //2. Find all leafs
+            var leafs = FindAllLeafs(nodes);
+            Console.WriteLine("Leafs: ");
+            foreach (var leaf in leafs)
+            {
+                Console.Write("{0}, ", leaf.Value);
+            }
+            Console.WriteLine();
+        }
+
+        private static List<Node<int>> FindAllLeafs(Node<int>[] nodes)
+        {
+            List<Node<int>> leafs = new List<Node<int>>();
+
+            foreach (var node in nodes)
+            {
+                if (node.Children.Count == 0)
+                {
+                    leafs.Add(node);
+                }
+            }
+
+            return leafs;
+        }
+
+        //3. Find all middle nodes
+
+
+    }
+}
---
+++ DataStructures&Algorithms/02-TreesAndTraversals/01-TreeTraversal/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeTraversal
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+
+        public List<Node<T>> Children { get; set; }
+
+        public bool HasParent { get; set; }
+
+        public Node()
+        {
+            this.Children = new List<Node<T>>();
+        }
+
+        public Node(T value)
+            : this()
+        {
+            this.Value = value;
+        }
+    }
+}
