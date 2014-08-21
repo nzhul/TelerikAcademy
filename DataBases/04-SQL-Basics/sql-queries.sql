2559ba9bcdc0c215c849c9054b965431888bf92e
nzhul <nzhul@bitbucket.org>
2014-08-21 17:09:26 +0300

    sql basics

--- DataBases/04-SQL-Basics/sql-queries.sql
+++ DataBases/04-SQL-Basics/sql-queries.sql
@@ -91,16 +91,34 @@ ON e.AddressID = a.AddressID
 
 
 -- 22.Write a SQL query to find all departments and all town names as a single list. Use UNION.
-
+SELECT Name as Name
+FROM Departments
+UNION
+SELECT Name as Name
+FROM Towns
+UNION
+SELECT AddressText
+FROM Addresses
+ORDER BY Name
 
 
 -- 23. Write a SQL query to find all the employees and the manager for each of them along with 
 -- 	   the employees that do not have manager. Use right outer join. Rewrite the query to use left outer join.
+SELECT a.FirstName AS Subbordinate, e.FirstName AS Boss
+FROM Employees e FULL OUTER JOIN Employees a
+ON e.EmployeeID = a.ManagerID;
 
+SELECT a.FirstName AS Subbordinate, e.FirstName AS Boss
+FROM Employees e LEFT OUTER JOIN Employees a
+ON e.EmployeeID = a.ManagerID
+ORDER BY e.FirstName;
 
 
 -- 24. Write a SQL query to find the names of all employees from the departments "Sales" and "Finance" whose hire year is between 1995 and 2005.
-
+SELECT e.FirstName + '' + e.LastName AS FullName, e.HireDate, d.Name
+FROM Employees e JOIN Departments d
+ON e.DepartmentID = d.DepartmentID
+WHERE d.Name IN ('Sales', 'Finance') AND HireDate > '1995-01-19 00:00:00' AND HireDate < '2005-01-19 00:00:00';
 
 
 
