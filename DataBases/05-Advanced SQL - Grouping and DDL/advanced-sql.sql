75176aceebc6bc9e07256c5c6fe09c1f4c0fea42
nzhul <nzhul@bitbucket.org>
2014-08-22 17:54:56 +0300

    up

--- DataBases/05-Advanced SQL - Grouping and DDL/advanced-sql.sql
+++ DataBases/05-Advanced SQL - Grouping and DDL/advanced-sql.sql
@@ -39,27 +39,53 @@ ON e.DepartmentID = d.DepartmentID
 WHERE d.Name = 'Sales'
 
 --07 Write a SQL query to find the number of all employees that have manager.
-
+SELECT COUNT(*)
+FROM Employees
+WHERE ManagerID IS NOT NULL
 
 --08 Write a SQL query to find the number of all employees that have no manager.
-
+SELECT COUNT(*)
+FROM Employees
+WHERE ManagerID IS NULL
 
 --09 Write a SQL query to find all departments and the average salary for each of them.
-
+SELECT Name, AVG(Salary) as AverageSalaryForDepartment
+FROM Employees e INNER JOIN Departments d
+ON e.DepartmentID = d.DepartmentID
+GROUP BY d.Name
 
 --10 Write a SQL query to find the count of all employees in each department and for each town.
-
+SELECT d.Name, t.Name, COUNT(EmployeeID) as EmployeeCount
+FROM Employees e JOIN Departments d
+ON e.DepartmentID = d.DepartmentID
+JOIN Addresses ad
+ON e.AddressID = ad.AddressID
+JOIN Towns t
+ON t.TownID = ad.TownID
+GROUP BY d.Name, t.Name
 
 --11 Write a SQL query to find all managers that have exactly 5 employees. Display their first name and last name.
+SELECT e.FirstName + ' ' + e.LastName as Manager
+FROM Employees e
+WHERE 5 = 
+	(
+		SELECT COUNT(*) FROM Employees m WHERE m.ManagerID = e.ManagerID 
+	)
 
 
 --12 Write a SQL query to find all employees along with their managers. For employees that do not have manager display the value "(no manager)".
-
+SELECT m.FirstName + ' ' + m.LastName as Subordinate, COALESCE(e.FirstName, '(no manager)') as Manager
+FROM Employees e RIGHT OUTER JOIN Employees m
+ON e.EmployeeID = m.ManagerID
 
 --13 Write a SQL query to find the names of all employees whose last name is exactly 5 characters long. Use the built-in LEN(str) function.
+SELECT FirstName + ' ' + LastName as FullName 
+FROM Employees
+WHERE LEN(LastName) = 5
 
 
 --14 Write a SQL query to display the current date and time in the following format "day.month.year hour:minutes:seconds:milliseconds". Search in  Google to find how to format dates in SQL Server.
+ 
 
 
 --15 Write a SQL statement to create a table Users. Users should have username, password, full name and last login time. Choose appropriate data types for the table fields. Define a primary key column with a primary key constraint. Define the primary key column as identity to facilitate inserting records. Define unique constraint to avoid repeating usernames. Define a check constraint to ensure the password is at least 5 characters long.
