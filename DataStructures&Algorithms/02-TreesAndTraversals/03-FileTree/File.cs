73428dfde688971499f7f2b0c9afa35701fe7451
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-25 20:54:43 +0300

    up

---
+++ DataStructures&Algorithms/02-TreesAndTraversals/03-FileTree/File.cs
@@ -0,0 +1,13 @@
+﻿namespace FileTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class File
+    {
+
+    }
+}
---
+++ DataStructures&Algorithms/02-TreesAndTraversals/03-FileTree/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FileTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
