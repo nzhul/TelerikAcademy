2bfee75fdae9b44e667c0a2131976ff2abf111b9
nzhul <nzhul@bitbucket.org>
2014-08-26 17:25:09 +0300

    ado net start

---
+++ DataBases/07-ADO-NET/01-CategoriesRowCount/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace CategoriesRowCount
+{
+    using CategoriesRowCount.Properties;
+    using System.Data.SqlClient;
+
+    public class Program
+    {
+        static void Main()
+        {
+            SqlConnection db = 
+                new SqlConnection("Server=SQLEXPRESS; Database=NORTHWIND; Integrated Security=true");
+
+            db.Open();
+            using (db)
+            {
+                SqlCommand rowCount = new SqlCommand("SELECT COUNT(*) FROM Categories", db);
+                System.Console.WriteLine("Number of rows in Categories Table: {0}", rowCount.ExecuteScalar());
+            }
+        }
+    }
+}
