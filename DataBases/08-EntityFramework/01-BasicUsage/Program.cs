ff977c67f32b2c16a29d76b7d3ccfa6936c4fda3
nzhul <nzhul@bitbucket.org>
2014-08-27 15:04:28 +0300

    entity start

---
+++ DataBases/08-EntityFramework/01-BasicUsage/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace BasicUsage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NorthwindDataModel.Data;
+
+    class Program
+    {
+        static void Main()
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            using (db)
+            {
+                //foreach (var customer in db.Customers)
+                //{
+                //    Console.WriteLine(customer.City);
+                //}
+
+                var customers =
+                    from c in db.Customers
+                    where c.City == "London"
+                    select c;
+
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine("{0} from -> {1}", customer.ContactName ,customer.City);
+                }
+            }
+        }
+    }
+}
