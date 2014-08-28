16c27c277d34b9d96aa05d9387364a8c8ac6bc74
nzhul <nzhul@bitbucket.org>
2014-08-28 12:43:31 +0300

    update database

---
+++ DataBases/08-EntityFramework/04-FindCustomerWithSQL/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FindCustomerWithSQL
+{
+    using System;
+    using NorthwindDataModel.Data;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            FindAllCustomers(1997, "Canada");
+        }
+
+        private static void FindAllCustomers(int orderDate, string shipDestination)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            using (db)
+            {
+                string customersQuery = @"SELECT c.ContactName from Customers
+                                    c INNER JOIN Orders o ON o.CustomerID = c.CustomerID 
+                                    WHERE (YEAR(o.OrderDate) = {0} AND o.ShipCountry = {1});";
+                object[] queryParams = { orderDate, shipDestination };
+
+                var customersResult = db.Database.SqlQuery<string>(customersQuery, queryParams);
+
+                foreach (var customer in customersResult)
+                {
+                    Console.WriteLine("Customer: {0}", customer);
+                }
+            }
+        }
+    }
+}
---
+++ DataBases/08-EntityFramework/05-FindAllSales/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace FindAllSales
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NorthwindDataModel.Data;
+
+    public class Program
+    {
+        static void Main()
+        {
+            FindSales(1996, 1996, "AK");
+        }
+
+        private static void FindSales(int startDate, int endDate, string region)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            using (db)
+            {
+                var searchResult =
+                    from order in db.Orders
+                    join orderDetails in db.Order_Details
+                    on order.OrderID equals orderDetails.OrderID
+                    where order.ShipRegion == region
+                    && order.OrderDate.Value.Year == startDate
+                    && order.ShippedDate.Value.Year == endDate
+                    select new
+                    {
+                        Amount = orderDetails.Quantity,
+                        Region = order.ShipRegion,
+                    };
+
+                foreach (var customer in searchResult)
+                {
+                    Console.WriteLine("Amount: {0}, Region: {1}", customer.Amount, customer.Region);
+                }
+            }
+        }
+    }
+}
