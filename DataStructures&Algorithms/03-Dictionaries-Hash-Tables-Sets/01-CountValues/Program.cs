e41b11d766a897f141b4f45bdba921f408504329
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-08-31 20:49:42 +0300

    Dictionaries some tasks

---
+++ DataStructures&Algorithms/03-Dictionaries-Hash-Tables-Sets/01-CountValues/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _01_CountValues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<double, int> countDict = new Dictionary<double, int>();
+            var numbers = new List<double>() { 3, 4, 4, -2.5, 3, 3, 4, 3, -2.5 };
+
+            foreach (var number in numbers)
+            {
+                if (countDict.ContainsKey(number))
+                {
+                    countDict[number]++;
+                }
+                else
+                {
+                    countDict.Add(number, 1);
+                }
+            }
+
+            foreach (var count in countDict)
+            {
+                Console.WriteLine("{0} -> {1} times", count.Key, count.Value);
+            }
+        }
+    }
+}
---
+++ DataStructures&Algorithms/03-Dictionaries-Hash-Tables-Sets/02-OddNumberOfTimes/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _02_OddNumberOfTimes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static void Main()
+        {
+            List<string> words = new List<string>() { "C#", "SQL", "PHP", "PHP", "SQL", "SQL", "SQL" };
+            Dictionary<string, int> wordOccurances = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (wordOccurances.ContainsKey(word))
+                {
+                    wordOccurances[word]++;
+                }
+                else
+                {
+                    wordOccurances.Add(word, 1);
+                }
+            }
+
+            foreach (var wordOccurance in wordOccurances)
+            {
+                if (wordOccurance.Value % 2 == 0)
+                {
+                    Console.WriteLine("{0} -> {1} times", wordOccurance.Key, wordOccurance.Value);
+                }
+            }
+
+
+        }
+    }
+}
