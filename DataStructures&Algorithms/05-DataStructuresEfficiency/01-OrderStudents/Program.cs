f05bbecb55dccf61992749da63810df400b8389c
nzhul <nzhul@bitbucket.org>
2014-09-01 16:57:48 +0300

    DataStructure Efficiency

---
+++ DataStructures&Algorithms/05-DataStructuresEfficiency/01-OrderStudents/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace DataStructuresEfficiency
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Wintellect.PowerCollections;
+
+    class Program
+    {
+        static void Main()
+        {
+            SortedDictionary<string, OrderedBag<Student>> students = new SortedDictionary<string, OrderedBag<Student>>();
+
+            string line;
+            using (StreamReader reader = new StreamReader(@"../../students.txt"))
+            {
+                while ((line = reader.ReadLine())!= null)
+                {
+                    string[] content = line.Split(new char[]{'|'}, StringSplitOptions.RemoveEmptyEntries);
+                    Student currentStudent = new Student(content[0].Trim(), content[1].Trim(), content[2]);
+                    if (students.ContainsKey(currentStudent.Course))
+                    {
+                        students[currentStudent.Course].Add(currentStudent);
+                    }
+                    else
+                    {
+                        students.Add(currentStudent.Course, new OrderedBag<Student>() { currentStudent });
+                    }
+                }
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("{0}: {1}", student.Key, string.Join(", ", student.Value));
+            }
+        }
+    }
+}
---
+++ DataStructures&Algorithms/05-DataStructuresEfficiency/01-OrderStudents/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresEfficiency
+{
+    class Student : IComparable<Student>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Course { get; set; }
+
+        public Student(string firstName, string lastName, string course)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Course = course;
+        }
+        
+        public int CompareTo(Student other)
+        {
+            if (this.FirstName != other.FirstName)
+            {
+                return StringComparer.CurrentCulture.Compare(this.FirstName, other.FirstName);
+            }
+            else if (this.LastName != other.LastName)
+            {
+                return StringComparer.CurrentCulture.Compare(this.LastName, other.LastName);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", FirstName, LastName);
+        }
+    }
+}
