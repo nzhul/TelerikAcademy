0cd4e4f097bc7c493641a21a427a07427f2cc7b2
nzhul <dobromirivanov1@gmail.com>
2014-09-02 15:17:02 +0300

    DSA BiDict

--- DataStructures&Algorithms/05-DataStructuresEfficiency/03-BiDictionary/BiDictionary.cs
+++ DataStructures&Algorithms/05-DataStructuresEfficiency/03-BiDictionary/BiDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,16 +8,15 @@ using Wintellect.PowerCollections;
 
 namespace BiDictionary
 {
-    public class BiDictionary<TKey1, Tkey2, TValue>
+    public class BiDictionary<TKey1, TKey2, TValue>
     {
         private class Entry : IEquatable<Entry>
         {
+            public TKey1 Key1 { get; private set; }
+            public TKey2 Key2 { get; private set; }
+            public TValue Value { get; private set; }
 
-            public TKey1 Key1 { get; set; }
-            public Tkey2 Key2 { get; set; }
-            public TValue Value { get; set; }
-
-            public Entry(TKey1 key1, Tkey2 key2, TValue value)
+            public Entry(TKey1 key1, TKey2 key2, TValue value)
             {
                 this.Key1 = key1;
                 this.Key2 = key2;
@@ -45,13 +45,106 @@ namespace BiDictionary
                     hashCode = (hashCode * 397) ^ this.Key1.GetHashCode();
                     hashCode = (hashCode * 397) ^ this.Key2.GetHashCode();
                     hashCode = (hashCode * 397) ^ this.Value.GetHashCode();
+
                     return hashCode;
                 }
             }
         }
 
         private readonly MultiDictionary<TKey1, Entry> byKey1 = null;
+        private readonly MultiDictionary<TKey2, Entry> byKey2 = null;
+        private readonly MultiDictionary<Tuple<TKey1, TKey2>, Entry> byKey1Key2 = null;
 
+        public BiDictionary(bool allowDuplicateValues)
+        {
+            this.byKey1 = new MultiDictionary<TKey1, Entry>(allowDuplicateValues);
+            this.byKey2 = new MultiDictionary<TKey2, Entry>(allowDuplicateValues);
+            this.byKey1Key2 = new MultiDictionary<Tuple<TKey1, TKey2>, Entry>(allowDuplicateValues);
+        }
 
+        public int Count
+        {
+            get
+            {
+                Debug.Assert(
+                    byKey1.KeyValuePairs.Count == byKey1Key2.KeyValuePairs.Count &&
+                    byKey2.KeyValuePairs.Count == byKey1Key2.KeyValuePairs.Count
+                );
+
+                return this.byKey1Key2.KeyValuePairs.Count;
+            }
+        }
+
+        public void Add(TKey1 key1, TKey2 key2, TValue value)
+        {
+            var entry = new Entry(key1, key2, value);
+
+            this.byKey1.Add(key1, entry);
+            this.byKey2.Add(key2, entry);
+
+            var key1key2 = new Tuple<TKey1, TKey2>(key1, key2);
+            this.byKey1Key2.Add(key1key2, entry);
+        }
+
+        public ICollection<TValue> GetByFirstKey(TKey1 key1)
+        {
+            return this.byKey1[key1].Select(entry => entry.Value).ToArray();
+        }
+
+        public void RemoveByFirstKey(TKey1 key1)
+        {
+            var entries = this.byKey1[key1];
+
+            foreach (var entry in entries)
+            {
+                this.byKey2.Remove(entry.Key2, entry);
+
+                var key1key2 = new Tuple<TKey1, TKey2>(entry.Key1, entry.Key2);
+                this.byKey1Key2.Remove(key1key2, entry);
+            }
+
+            this.byKey1.Remove(key1);
+        }
+
+        public ICollection<TValue> GetBySecondKey(TKey2 key2)
+        {
+            return this.byKey2[key2].Select(entry => entry.Value).ToArray();
+        }
+
+        public void RemoveBySecondKey(TKey2 key2)
+        {
+            var entries = this.byKey2[key2];
+
+            foreach (var entry in entries)
+            {
+                this.byKey1.Remove(entry.Key1, entry);
+
+                var key1key2 = new Tuple<TKey1, TKey2>(entry.Key1, entry.Key2);
+                this.byKey1Key2.Remove(key1key2, entry);
+            }
+
+            this.byKey2.Remove(key2);
+        }
+
+        public ICollection<TValue> GetByFirstAndSecondKey(TKey1 key1, TKey2 key2)
+        {
+            var key1key2 = new Tuple<TKey1, TKey2>(key1, key2);
+
+            return this.byKey1Key2[key1key2].Select(entry => entry.Value).ToArray();
+        }
+
+        public void RemoveByFirstAndSecondKey(TKey1 key1, TKey2 key2)
+        {
+            var key1key2 = new Tuple<TKey1, TKey2>(key1, key2);
+            var entries = this.byKey1Key2[key1key2];
+
+            foreach (var entry in entries)
+            {
+                this.byKey1.Remove(entry.Key1, entry);
+                this.byKey2.Remove(entry.Key2, entry);
+            }
+
+            this.byKey1Key2.Remove(key1key2);
+        }
     }
 }
--- DataStructures&Algorithms/05-DataStructuresEfficiency/03-BiDictionary/Program.cs
+++ DataStructures&Algorithms/05-DataStructuresEfficiency/03-BiDictionary/Program.cs
@@ -1,15 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BiDictionary
+﻿namespace BiDictionary
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            var bidict = new BiDictionary<string, int, string>(allowDuplicateValues: true);
+
+            bidict.Add("pesho", 1, "JavaScript");
+            bidict.Add("gosho", 2, "Java");
+            bidict.Add("nakov", 3, "C#");
+            bidict.Add("nakov", 3, "C#");
+            bidict.Add("gosho", 3, "Coffee");
+            bidict.Add("nakov", 1, "Python");
+
+            Console.WriteLine(string.Join(" ", bidict.GetByFirstKey("nakov")));
+            Console.WriteLine(string.Join(" ", bidict.GetBySecondKey(3)));
+            Console.WriteLine(string.Join(" ", bidict.GetByFirstAndSecondKey("nakov", 3)));
+
+            Console.WriteLine(bidict.Count);
+
+            bidict.RemoveByFirstKey("gosho");
+            Console.WriteLine(bidict.Count);
+
+            bidict.RemoveBySecondKey(3);
+            Console.WriteLine(bidict.Count);
+
+            bidict.RemoveByFirstAndSecondKey("nakov", 1);
+            Console.WriteLine(bidict.Count);
+
 
         }
     }
