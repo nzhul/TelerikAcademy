72589802e2c66f492bc4895e44483ee80ab13f2a
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-09-04 19:23:52 +0300

    up

--- DataBases/13-MongoDB-and-MongoDB-with-NET/00-TestingMongo/Program.cs
+++ DataBases/13-MongoDB-and-MongoDB-with-NET/00-TestingMongo/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace _TestingMongo
 {
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+    using MongoDB.Driver;
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -8,8 +11,48 @@
 
     class Program
     {
+        const string DatabaseHost = "mongodb://127.0.0.1";
+        const string DatabaseName = "Logger";
+
+        class Log
+        {
+            [BsonRepresentation(BsonType.ObjectId)]
+            public string Id { get; set; }
+
+            public string name { get; set; }
+        }
+
+
+
+        static MongoDatabase GetDatabase(string name, string fromHost)
+        {
+            var mongoClient = new MongoClient(fromHost);
+            var server = mongoClient.GetServer();
+            return server.GetDatabase(name);
+        }
         static void Main()
         {
+            var db = GetDatabase(DatabaseName, DatabaseHost);
+            var collection = db.GetCollection<Log>("Logs");
+
+            collection.FindAll().Select(log => string.Format("[{0}]-->{1}", log.Id, log.name))
+                .Print();
+
+
+
+
+
+            //vObjs.Insert(new Log() { 
+            //    Id = ObjectId.GenerateNewId().ToString(),
+            //    LogDate = DateTime.Now,
+            //    Text = "Some text from C#"
+            //});
+
+            //vObjs.Insert(new {
+            //    Id = ObjectId.GenerateNewId().ToString(),
+            //    Name = "Doncho1"
+            //});
+
 
         }
     }
