db4d20db86d9252b0fd0210cd097d958e33a1b39
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-09-10 21:03:11 +0300

    Combinatorics

---
+++ DataStructures&Algorithms/08-Combinatorics/01-BinaryPasswords/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BinaryPasswords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Numerics;
+    using System.Text;
+    using System.Threading.Tasks;
+    class Program
+    {
+        static void Main()
+        {
+            string line = Console.ReadLine();
+            BigInteger counter = 0;
+
+            if (String.IsNullOrEmpty(line))
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == '*')
+                {
+                    counter++;
+                }
+            }
+
+            BigInteger combinations = (BigInteger)Math.Pow(2, (ulong)counter);
+            Console.WriteLine(combinations);
+
+        }
+    }
+}
