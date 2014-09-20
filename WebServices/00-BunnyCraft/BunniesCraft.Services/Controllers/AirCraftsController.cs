7f70a3de63fe6556fb3e7486965b69f2a1f25cea
Dobromir Ivanov <dobromirivanov1@gmail.com>
2014-09-20 12:59:30 +0300

    up

--- WebServices/00-BunnyCraft/BunniesCraft.Services/Controllers/AirCraftsController.cs
+++ WebServices/00-BunnyCraft/BunniesCraft.Services/Controllers/AirCraftsController.cs
@@ -8,15 +8,17 @@
     public class AirCraftsController : ApiController
     {
         private IRepository<AirCraft> aircrafts;
+        private IRepository<Bunny> bunnies;
 
         public AirCraftsController()
-            : this(new Repository<AirCraft>())
+            : this(new Repository<AirCraft>(), new Repository<Bunny>())
         {
 
         }
-        public AirCraftsController(IRepository<AirCraft> aircrafts)
+        public AirCraftsController(IRepository<AirCraft> airCrafts, IRepository<Bunny> bunnies)
         {
-            this.aircrafts = aircrafts;
+            this.aircrafts = airCrafts;
+            this.bunnies = bunnies;
         }
 
         [HttpGet]
@@ -31,7 +33,7 @@
         }
 
         [HttpPost]
-        public IHttpActionResult Create(AirCraftModel aircraft)
+        public IHttpActionResult Create(AirCraftModel airCraft)
         {
             if (!this.ModelState.IsValid)
             {
@@ -40,12 +42,71 @@
 
             var newAirCraft = new AirCraft
             {
-                Model = aircraft.Model
+                Model = airCraft.Model
             };
 
             this.aircrafts.Add(newAirCraft);
             this.aircrafts.SaveChanges();
+
+            airCraft.Id = newAirCraft.Id;
             return Ok(newAirCraft);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Update(int id, AirCraftModel airCraft)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingAirCraft = this.aircrafts.All().FirstOrDefault(a => a.Id == id);
+            if (existingAirCraft == null)
+            {
+                return BadRequest("Such aircraft does not exists!");
+            }
+
+            existingAirCraft.Model = airCraft.Model;
+            this.aircrafts.SaveChanges();
+
+            return Ok(airCraft);
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Delete(int id)
+        {
+            var existingAirCraft = this.aircrafts.All().FirstOrDefault(a => a.Id == id);
+            if (existingAirCraft == null)
+            {
+                return BadRequest("Such aircraft does not exists!");
+            }
+
+            this.aircrafts.Delete(existingAirCraft);
+            this.aircrafts.SaveChanges();
+
+            return Ok();
+        }
+
+        [HttpPost]
+        public IHttpActionResult AddBunny(int id, int bunnyId)
+        {
+            var theAirCraft = this.aircrafts.All().FirstOrDefault(a => a.Id == id);
+            if (theAirCraft == null)
+            {
+                return BadRequest("Such airCraft does not exists! - invalid ID");
+            }
+
+            var theBunny = this.bunnies.All().FirstOrDefault(b => b.Id == id);
+            if (theBunny == null)
+            {
+                return BadRequest("Such bunny does not exists! - invalid ID");
+            }
+
+            //theAirCraft.Bunnies.Add(theBunny);
+            theBunny.AircraftId = id;
+            this.bunnies.SaveChanges();
+            
+            return Ok();
         }
     }
 }
--- WebServices/00-BunnyCraft/BunniesCraft.Services/Controllers/BunniesController.cs
+++ WebServices/00-BunnyCraft/BunniesCraft.Services/Controllers/BunniesController.cs
@@ -1,17 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using BunniesCraft.Data.Repositories;
+using BunniesCraft.Models;
 using System.Web.Http;
+using System.Linq;
+using BunniesCraft.Services.Models;
 
 namespace BunniesCraft.Services.Controllers
 {
     public class BunniesController : ApiController
     {
-        public int GetBunnies()
+        private IRepository<Bunny> bunnies;
+
+        public BunniesController()
+            : this(new Repository<Bunny>())
         {
-            return 1;
+
+        }
+
+        public BunniesController(IRepository<Bunny> bunnies)
+        {
+            this.bunnies = bunnies;
+        }
+
+        [HttpGet]
+        public IHttpActionResult All()
+        {
+            var bunnies = this.bunnies.All().Select(b => new BunnyModel
+            {
+                Id = b.Id,
+                Name = b.Name,
+                Health = b.Health,
+                Color = b.Color
+            });
+            return Ok(bunnies);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Create(BunnyModel bunny)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                var newBunny = new Bunny
+                {
+                    Name = bunny.Name,
+                    Health = bunny.Health,
+                    Color = bunny.Color,
+                };
+
+                this.bunnies.Add(newBunny);
+                this.bunnies.SaveChanges();
+
+                bunny.Id = newBunny.Id;
+                return Ok(newBunny);
+            }
         }
     }
 }
